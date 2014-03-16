00adeef449d0e39b2392c74bcb352db413ddea3c
adamblake <adamblake@users.noreply.github.com>
2014-03-16 13:14:24 -0700

    tweaked button style removed superfluous code and updated layout

--- Code/TrialTypes/Instruct.php
+++ Code/TrialTypes/Instruct.php
@@ -1,14 +1,15 @@
 <?php
-	trialTiming();					// determines timing and user/computer timing mode
-	
-	echo '<div id="centerContent" class="PreCache">
-			<div class="Instruct">'. $currentTrial['Procedure']['Procedure Notes'].'</div>
-		  </div>';
-	// the hidden form below collects RT and displays the 'Done' button for user timed trials
-	echo '<div id="buttPos" class="PreCache">
-			<form class="'.$formClass.'" action="postTrial.php" method="post">
-				<input	name="RT"	type="text"		value=""	class="RT Hidden"	/>
-				<input	id="FormSubmitButton"	type="submit"	value="Done"	/>
+	// determines timing and user/computer timing mode
+	trialTiming();
+
+	// show stim
+	echo '<div>'. $currentTrial['Procedure']['Procedure Notes'].'</div>';
+
+	// include form to collect RT and advance page
+	echo '<div class="precache textright">
+			<form class="'.$formClass.'" action="postTrial.php" method=post>
+				<input class=hidden id=RT name=RT type=text value="" />
+				<input class="button button-trial-advance" id=FormSubmitButton type=submit value="Next"" />
 			</form>
 		  </div>';
 ?>
