159749e81f371c23f987e2e94e46df71187d5ea3
adamblake <adamblake@users.noreply.github.com>
2014-03-17 01:11:08 -0700

    study styles

--- Code/TrialTypes/Study.php
+++ Code/TrialTypes/Study.php
@@ -2,14 +2,14 @@
 	$compTime = 5;					// time in seconds to use for 'computer' timing
 	trialTiming();					// determines timing and user/computer timing mode
 
-	echo '<div class="precache textcenter">
-			<span class="left">'.$cue.'</span>
-			<span class="divider">:</span>
-			<span class="right">'.$target.'</span>
-		  </div>';
+	echo "<div class='study precache'>
+			<span class=study-left>    {$cue}   </span>
+			<span class=study-divider>    :     </span>
+			<span class=study-right>  {$target} </span>
+		  </div>";
 
     // include form to collect RT and advance page
-    echo '<div class="precache textright">
+    echo '<div class="precache textcenter">
             <form class="'.$formClass.'" action="postTrial.php" method=post>
                 <input class=hidden id=RT name=RT type=text value="" />
                 <input class="button button-trial-advance" id=FormSubmitButton type=submit value="Next"" />
