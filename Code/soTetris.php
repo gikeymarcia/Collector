949b9b92ff0c655f153dfc3872e78a597ab4efe0
adamblake <adamblake@users.noreply.github.com>
2014-03-19 16:53:30 -0700

    Reworked the whole tetris stepout New styles Cleaner instructions JS on own sheet

--- Code/soTetris.php
+++ Code/soTetris.php
@@ -5,7 +5,7 @@
  */
 	require 'CustomFunctions.php';							// Load custom PHP functions
 	initiateCollector();
-	
+
 	$currentPos		=& $_SESSION['Position'];
 	$currentTrial	=& $_SESSION['Trials'][$currentPos];
 	// pulls in the specified tetris time from the order file, column "Timing"
@@ -19,73 +19,72 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link href="css/global.css" rel="stylesheet" type="text/css" />
-	<link href='http://fonts.googleapis.com/css?family=Kreon' rel='stylesheet' type='text/css' />
-	<link href='http://fonts.googleapis.com/css?family=Orbitron:700' rel='stylesheet' type='text/css'>
 	<title>Tetris</title>
 </head>
 <?php flush(); ?>
 <body>
-	<div id="Clock"><span>Seconds remaining</span>
-		<div class="Countdown"></div>
+	<div class=stepout-clock>
+	    <span>Seconds remaining</span>
+		<h3 class=countdown></h3>
 	</div>
-	
-	<h1 class="stepout">Tetris!</h1>
-	
-	<div id="StepoutInstructions">
-			<p>Try to achieve the highest score possible</p>
-			<p>Move the pieces from side to side using the <b>left</b> and <b>right arrows</b>.<br />
-				Press the <b>X</b> or the <b>Up arrow</b> to rotate right; press <b>Z</b> to rotate left. <br />
-				Drop the piece into place with the <b>space bar</b></p>
+
+	<div class='tetris-wrap textcenter'>
+        <h3>Play a quick game of Tetris while we load the next part</h3>
+        <div class='grid tetris-controls'>
+            <div class='grid-item'>
+                <h2>Controls</h2>
+            </div><!--
+         --><div class='grid-item grid-1-3'>
+                <p>Move:</p>
+            </div><!--
+         --><div class='grid-item grid-2-3'>
+                <p><strong>Left</strong> and <strong>Right arrows</strong></p>
+            </div><!--
+         --><div class='grid-item grid-1-3'>
+                <p>Rotate Right:</p>
+            </div><!--
+         --><div class='grid-item grid-2-3'>
+                <p><strong>X</strong> or <strong>Up arrow</strong></p>
+            </div><!--
+         --><div class='grid-item grid-1-3'>
+                <p>Rotate Left:</p>
+            </div><!--
+         --><div class='grid-item grid-2-3'>
+                <p><strong>Z</strong></p>
+            </div><!--
+         --><div class='grid-item grid-1-3'>
+                <p>Drop:</p>
+            </div><!--
+         --><div class='grid-item grid-2-3'>
+                <p><strong>Spacebar</strong></p>
+            </div>
+        </div>
+        <div class=button id=reveal>Start</div>
+        <div class=tetris>
+            <embed src="http://www.cogfog.com/nblox.swf" menu=false width=550 height=650
+            quality=high type="application/x-shockwave-flash"
+            pluginspage="http://www.macromedia.com/go/getflashplayer/" />
+        </div>
 	</div>
-	
-	
-	<div id="Tetris">
-		<EMBED src="http://www.cogfog.com/nblox.swf" menu="false" width="550" height="650"
-		quality="high" TYPE="application/x-shockwave-flash" 
-		PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer/" />
-	</div>
-			
-	
-	<?php
-		echo '<div id="time" class="Hidden">'.$time.'</div>';
-		
-		// this hidden and empty form is submitted to progress the page
-		echo '<form id="loadingForm" action="postTrial.php" method="get"> </form>';
-		
-		#### Pre-cache next trial
-		echo '<div class="Hidden">';
-			echo show($_SESSION['Trials'][$_SESSION['Position']+1]['Stimuli']['Cue']);
-			echo show($_SESSION['Trials'][$_SESSION['Position']+1]['Stimuli']['Target']);
-			echo show($_SESSION['Trials'][$_SESSION['Position']+1]['Stimuli']['Answer']);
-		echo '</div>';
-	?>
-	
-	<script src="http://code.jquery.com/jquery-1.8.0.min.js" type="text/javascript"> </script>
-	<script type="text/javascript">
-		var timer = $("#time").html();
-		var interval = 1000;
-		$(".Countdown").html(timer);
-		
-		setInterval(countdown,interval);
-		
-		function countdown() {
-			timer = timer-1;
-			if(timer >= 0) {
-				$(".Countdown").html(timer);
-			}
-			if(timer == 5) {
-				// hide these things when timer hits 5 seconds
-				$("EMBED").addClass("Hidden");
-				$("#Clock").addClass("Hidden");
-				$("#StepoutInstructions").addClass("Hidden");
-				$("h1").addClass("Hidden");
-				$("#Tetris").html("<div class=\"cont\">Get ready to continue in ...  <div class=\"Countdown\">5</div></div>");
-			}
-			if(timer <= 0) {
-				$('#loadingForm').submit();
-			}
-		}
-	</script>
+
+
+    <!-- used to set timer -->
+	<div id=Time class=hidden><?php echo $time; ?></div>
+
+	<!-- hidden form to advance page -->
+	<form id=loadingForm action="postTrial.php" method=get></form>
+
+    <!-- Pre-Cache Next trial -->
+    <div class=precachenext>
+        <?php
+        echo show($nextTrial['Stimuli']['Cue']).'   <br />';
+        echo show($nextTrial['Stimuli']['Target']).'<br />';
+        echo show($nextTrial['Stimuli']['Answer']).'<br />';
+        ?>
+    </div>
+
+	<script src="http://code.jquery.com/jquery-1.8.0.min.js" type="text/javascript"></script>
+    <script src="javascript/tetris.js" type="text/javascript"></script>
 
 </body>
 </html>
