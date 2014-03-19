d1dcb8afe792d76fac9683e413dc752536f5142a
adamblake <adamblake@users.noreply.github.com>
2014-03-19 14:28:53 -0700

    updated last of trial types (studypic)

--- Code/TrialTypes/StudyPic.php
+++ Code/TrialTypes/StudyPic.php
@@ -1,17 +1,18 @@
 <?php
-	$compTime = 5;					// time in seconds to use for 'computer' timing
-	trialTiming();					// determines timing and user/computer timing mode
+    $compTime = 5;                  // time in seconds to use for 'computer' timing
+    trialTiming();                  // determines timing and user/computer timing mode
+?>
+    <!-- show the image -->
+    <div class='precache pic'>
+        <?php echo show($cue); ?>
+    </div>
+    <!-- show the target -->
+    <h2 class="precache textcenter"><?php echo $target; ?></h2>
 
-	echo '<div class="pic PreCache">
-			'. show($cue).
-		 '</div>';
-	echo '<div class="picWord PreCache">'.$target.'</div>';
-	$formClass = $formClass.' center';
-
-    // include form to collect RT and advance page
-    echo '<div class="precache textright">
-            <form class="'.$formClass.'" action="postTrial.php" method=post>
-                <input class=hidden id=RT name=RT type=text value="" />
-                <input class="button button-trial-advance" id=FormSubmitButton type=submit value="Next"" />
-            </form>
-          </div>';
+    <!-- response and RT form -->
+    <div class="precache textcenter">
+        <form class="<?php echo $formClass; ?>" action="postTrial.php" method=post>
+            <input class=hidden id=RT name=RT type=text value="" />
+            <input class="button button-trial-advance" id=FormSubmitButton type=submit value="Next"" />
+        </form>
+    </div>
