40af71a80eddbefe8793e59cb4ce764e38a8a7f7
Adam Blake <adamblake@g.ucla.edu>
2015-01-19 19:24:35 -0800

    Updates some CSS and HTML to be more semantic
    Basic Info was completely reworked along with the basic Forms CSS to both abstract the forms classes and make more semantically intuitive HTML All form elements are wrapped in either the collector-form class or the collector-form-element class.
    
    The Informed Consent area has been changed to a DIV to allow for easier styling and to remove the cursor and tab-in.
    
    Page wrapper was added to make vertical alignments easier with CSS3's transform function. **Needs testing in all browsers.** Only the index page has been tested. Other pages need respective SECTION elements to reside within.

--- Code/BasicInfo.php
+++ Code/BasicInfo.php
@@ -8,137 +8,174 @@
     $title = 'Basic Information';
     require $_codeF . 'Header.php';
 ?>
-<div class="main-contain">
-	<h2 class="textcenter">Basic Information</h2>
 
-	<form name="Demographics" class="collector-form collector-form-extra" action="BasicInfoData.php" method="post" autocomplete="off">
-
-		<div class="field">
-			<legend>What is your gender?</legend>
-			<input type="radio"   value="Male"     class="radio"    name="Gender"    />    Male     <br />
-			<input type="radio"   value="Female"   class="radio"    name="Gender"    />    Female   <br />
-		</div>
-
-		<div class="field">
-			<p>What is your age?</p>
-			<input type="text" value="" name="Age" autocomplete="off" class="forceNumeric" />
-		</div>
-
-		<div class="field">
-			<p>Which of the following best describes your highest achieved education level?</p>
-			<select name="Education">
-				<option selected="selected">Select Level</option>
-				<option>    Some High School                            </option>
-				<option>    High School Graduate                        </option>
-				<option>    Some college, no degree                     </option>
-				<option>    Associates degree                           </option>
-				<option>    Bachelors degree                            </option>
-				<option>    Graduate degree (Masters, Doctorate, etc.)  </option>
-			</select>
-		</div>
-
-		<div class="field">
-			<p>Are you Hispanic?</p>
-			<input    type="radio"    name="Hispanic"    value="Yes" />    Yes    <br />
-			<input    type="radio"    name="Hispanic"    value="No"  />    No     <br />
-		</div>
-
-		<div class="field">
-			<p>Which of the following best describes your ethnicity?</p>
-			<select name="Race">
-				<option selected="selected">Select Level</option>
-				<option>	American Indian/Alaskan Native		</option>
-				<option>	Asian/Pacific Islander				</option>
-				<option>	Black           					</option>
-				<option>	White                       		</option>
-				<option>	Other/unknwon						</option>
-			</select>
-		</div>
-
-		<div class="field">
-			<p>Do you speak English fluently?</p>
-			<input    type="radio"    name="English"    value="Fluent"        />    Yes, I am fluent in English     <br />
-			<input    type="radio"    name="English"    value="Non-Fluent"    />    No, I am not fluent in English  <br />
-		</div>
+<section class="basicinfo content clearfix">
+<form class="collector-form" name="Demographics" action="BasicInfoData.php" method="post" autocomplete="off">
+  <fieldset>
+    <legend>Basic Information</legend>
+    <div class="collector-form-element">
+      <label>Gender</label>
+      <div class="radio">
+        <input id="male" name="Gender" type="radio" value="Male" required>
+        <label for="male" class="clearfix">Male</label>
+          
+        <input id="female" name="Gender" type="radio" value="Female" required>
+        <label for="female" class="clearfix">Female</label>
+          
+        <input id="other" name="Gender" type="radio" value="Other" required>
+        <label for="other">Other</label>  
+      </div>
+    </div>
+      
+    <div class="collector-form-element">
+      <label for="age">Age</label>
+      <input id="age" name="Age" type="text" pattern="[0-9][0-9]" value="" autocomplete="off" required>
+    </div>
+          
+    <div class="collector-form-element">
+      <label for="education">Highest level of education</label>
+      <select id="education" name="Education" required>
+        <option value="" default selected>Select Level</option>
+        <option>Some High School
+        <option>High School Graduate
+        <option>Some college, no degree
+        <option>Associates degree
+        <option>Bachelors degree
+        <option>Graduate degree (Masters, Doctorate, etc.)
+      </select>
+    </div>  
+    
+    <div class="collector-form-element">
+      <label>Are you Hispanic?</label>
+      <div class="radio">
+        <input id="hispanic_yes" name="Hispanic" type="radio" value="Yes" required>
+        <label for="hispanic_yes" class="clearfix">Yes</label>
         
-		<div class="field">
-			<p>What age did you start learning English? If English is your first language, please put 0.</p>
-			<input type="text" value="" name="AgeEnglish" autocomplete="off"/>
-		</div>
+        <input id="hispanic_no" name="Hispanic" type="radio" value="No" required>
+        <label for="hispanic_no">No</label>
+      </div>
+    </div>
+    
+    <div class="collector-form-element">
+      <label for="Race">Ethnicity</label>
+      <select name="Race" required>
+        <option value="" default selected>Select one</option>
+        <option>American Indian/Alaskan Native
+        <option>Asian/Pacific Islander
+        <option>Black
+        <option>White
+        <option>Other/unknown
+      </select>
+    </div>  
+    
+    <div class="collector-form-element">
+      <label>Do you speak English fluently?</label>
+      <div class="radio">
+        <input id="fluent_yes" name="Fluent" type="radio" value="Yes" required>
+        <label for="fluent_yes" class="clearfix">Yes</label>
+        
+        <input id="fluent_no" name="Fluent" type="radio" value="No" required>
+        <label for="fluent_no">No</label>
+      </div>  
+    </div>
+    
+    <div class="collector-form-element">
+      <label for="ageeng">At what age did you start learning English?</label>
+      <input id="ageeng" name="AgeEnglish" type="text" pattern="[0-9][0-9]?" value="" autocomplete="off" required>
+      <p class="labelsub">If English is your first language please enter 0.
+    </div>    
+    
+    <div class="collector-form-element">
+      <label for="country">Country</label>
+      <input id="country" name="Country" type="text" autocomplete="off" required>  
+    </div>
 
-		<div class="field">
-			<p>In what country do you live?</p>
-			<input type="text" value="" name="Country" size="30"    autocomplete="off" />
-		</div>
-		
-		<div class="field">
-			<p>If you live in the United States of America, which state do you live in?</p>
-			<select name="State">
-				<option selected="selected">Select State</option>
-				<option>Alabama			</option>
-				<option>Alaska			</option>
-				<option>Arizona			</option>
-				<option>Arkansas		</option>
-				<option>California		</option>
-				<option>Colorado		</option>
-				<option>Connecticut		</option>
-				<option>Delaware		</option>
-				<option>Florida			</option>
-				<option>Georgia			</option>
-				<option>Hawaii			</option>
-				<option>Idaho			</option>
-				<option>Illinois		</option>
-				<option>Indiana			</option>
-				<option>Iowa			</option>
-				<option>Kansas			</option>
-				<option>Kentucky		</option>
-				<option>Louisiana		</option>
-				<option>Maine			</option>
-				<option>Maryland		</option>
-				<option>Massachusetts	</option>
-				<option>Michigan		</option>
-				<option>Minnesota		</option>
-				<option>Mississippi		</option>
-				<option>Missouri		</option>
-				<option>Montana			</option>
-				<option>Nebraska		</option>
-				<option>Nevada			</option>
-				<option>New Hampshire	</option>
-				<option>New Jersey		</option>
-				<option>New Mexico		</option>
-				<option>New York		</option>
-				<option>North Carolina	</option>
-				<option>North Dakota	</option>
-				<option>Ohio			</option>
-				<option>Oklahoma		</option>
-				<option>Oregon			</option>
-				<option>Pennsylvania	</option>
-				<option>Rhode Island	</option>
-				<option>South Carolina	</option>
-				<option>South Dakota	</option>
-				<option>Tennessee		</option>
-				<option>Texas			</option>
-				<option>Utah			</option>
-				<option>Vermont			</option>
-				<option>Virginia		</option>
-				<option>Washington		</option>
-				<option>West Virginia	</option>
-				<option>Wisconsin		</option>
-				<option>Wyoming			</option>
-			</select>
-		</div>
+    <div class="collector-form-element">
+      <label for="State">If you are living in the US, which state?</label>
+      <select name="State">
+        <option value="" default selected>Select State
+        <option>Alabama
+        <option>Alaska
+        <option>Arizona
+        <option>Arkansas
+        <option>California
+        <option>Colorado
+        <option>Connecticut
+        <option>Delaware
+        <option>Florida
+        <option>Georgia
+        <option>Hawaii
+        <option>Idaho
+        <option>Illinois
+        <option>Indiana
+        <option>Iowa
+        <option>Kansas
+        <option>Kentucky
+        <option>Louisiana
+        <option>Maine
+        <option>Maryland
+        <option>Massachusetts
+        <option>Michigan
+        <option>Minnesota
+        <option>Mississippi
+        <option>Missouri
+        <option>Montana
+        <option>Nebraska
+        <option>Nevada
+        <option>New Hampshire
+        <option>New Jersey
+        <option>New Mexico
+        <option>New York
+        <option>North Carolina
+        <option>North Dakota
+        <option>Ohio
+        <option>Oklahoma
+        <option>Oregon
+        <option>Pennsylvania
+        <option>Rhode Island
+        <option>South Carolina
+        <option>South Dakota
+        <option>Tennessee
+        <option>Texas
+        <option>Utah
+        <option>Vermont
+        <option>Virginia
+        <option>Washington
+        <option>West Virginia
+        <option>Wisconsin
+        <option>Wyoming
+      </select>
+    </div>
+  </fieldset>
+  
+  <!-- ## SET ## Use this area to provide the equivalent of an informed consent form -->
+  <fieldset>
+    <legend>Informed Consent</legend>
+    <div class="collector-form-element consent">
+      <h1>Learning Words and Remembering Facts</h1>
+      <h2>Section 1</h2>
+      <p>This is the informed consent form. You can put whatever you want here.
+      <h2>Section 2</h2>
+      <h3>Part A</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+      <h3>Part B</h3>
+      <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+      <h2>Section 3</h2>
+      <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere
+    </div>
+  </fieldset>
+  
+  <div class="collector-form-element consent-check">
+    <input id="consent" name="Consent" type="checkbox">
+    <label for="consent">Check this box if you have read, understand, and agree to the Informed Consent above.</label>
+  </div>
+  
+  <div class="collector-form-element textcenter">
+    <input class="collector-button" type="submit" value="Submit Basic Info">
+  </div>
+</form>    
+</section>
 
-		<!-- ## SET ##  Use this area to provide the equivalent of an informed consent form -->
-		<div class="consent">
-			<h3 class="consent-legend">Informed Consent:</h3>
-			<h3 class="consent-legend textcenter">Learning Words and Remembering Facts</h3>
-			<textarea rows="20" cols="45" wrap="physical">This is the informed consent form.  You can put whatever you want here.</textarea>
-		</div>
-
-		<div class="consent textcenter">
-			<input class="button" type="submit" value="Submit Basic Info" />
-		</div>
-	</form>
-</div>
 <?php
     require $_codeF . 'Footer.php';
