ce9075ccebdd554bd380f9678046a8dada6d81f3
TysonKerr <tysonkerr117@gmail.com>
2015-03-03 10:09:19 -0800

    Index.php update:
    -added more info to condition selection
    -added some error checks to the conditions file
    
    closes #62

--- index.php
+++ index.php
@@ -20,7 +20,21 @@
     $_SESSION = array();                                // reset session so it doesn't contain any information from a previous login attempt
 
 	// load and sort conditions
-	$Conditions = GetFromFile($expFiles.$conditionsFileName);
+	$Conditions = GetFromFile($expFiles.$conditionsFileName, FALSE);
+    $conditionNumbers = array();
+    foreach ($Conditions as $cond) {
+        if (isset($conditionNumbers[$cond['Number']])) {
+            exit('Error: Multiple Conditions use the same number. Please check your "'.$conditionsFileName.'" file and make sure each number in the "Number" column is unique.');
+        } else {
+            $conditionNumbers[$cond['Number']] = TRUE;
+            if (!file_exists($expFiles . $stimF . $cond['Stimuli'])) {
+                exit('Error: The stimuli file "'   . $cond['Stimuli']   . '" could not be found in the ' . $stimF . ' subfolder of the ' . $expFiles . ' folder, for Condition ' . $cond['Number'] . ': ' . $cond['Condition Description'] . '. Either rename a file to "' . $cond['Stimuli']   . '" or change this entry in the "'.$conditionsFileName.'" file to match an existing file.');
+            }
+            if (!file_exists($expFiles . $procF . $cond['Procedure'])) {
+                exit('Error: The procedure file "' . $cond['Procedure'] . '" could not be found in the ' . $procF . ' subfolder of the ' . $expFiles . ' folder, for Condition ' . $cond['Number'] . ': ' . $cond['Condition Description'] . '. Either rename a file to "' . $cond['Procedure'] . '" or change this entry in the "'.$conditionsFileName.'" file to match an existing file.');
+            }
+        }
+    }
 	$tempCond   = SortByKey($Conditions, 'Number');
 	
 	$title = 'Experiment Login Page';
@@ -47,11 +61,12 @@
 				#### Display conditions as choices ####
 
 				// output all possible condition choices
-				for ($i=2; $i<count($tempCond); $i++) {
-                    $name  = $useConditionNames ? $tempCond[$i]['Condition Description'] : $tempCond[$i]['Number'];
-                    $title = $showConditionInfo ? $tempCond[$i]['Stimuli'] . ' - ' . $tempCond[$i]['Procedure'] : '';
-					echo '<option value="' . $tempCond[$i]['Number'] . '" title="' . $title . '">' . $name . '</option>';
-				}
+                foreach ($Conditions as $cond) {
+                    $name  = $useConditionNames ? $cond['Number']   . '. ' . $cond['Condition Description']         : $cond['Number'];
+                    $title = $showConditionInfo ? $stimF . $cond['Stimuli'] . ' - ' . $procF . $cond['Procedure']   : '';
+                    
+					echo '<option value="' . $cond['Number'] . '" title="' . $title . '">' . $name . '</option>';
+                }
 				?>
 			</select>
 
