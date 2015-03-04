bb7c4aae9a6869741c7e779a64946dcf28da440d
gikeymarcia <gikeymarcia@gmail.com>
2015-03-04 01:15:09 -0800

    considerably cleaned up ShuffleTester code
    added bar that shows table contents on hover (like the excel formula editor)
    bar contents can be locked in by clicking a cell. Unlocked by clicking again
    code is now thoroughly commented
    as far as I'm concerned this tool is done.

--- Tools/ShuffleTester/ShuffleTester.php
+++ Tools/ShuffleTester/ShuffleTester.php
@@ -5,8 +5,7 @@
     
     // needed for this tool
     $root = '../';                                      // gets us to the root
-    require_once $root . 'Code/advancedShuffles.php';   // nadvanced (Tyson) shuffles
-    
+    require_once $root . 'Code/advancedShuffles.php';   // advanced (Tyson) shuffles
     
     // making a place to store all variables (storing inside admin is best because it will be wiped on logout)
     $_SESSION['admin']['shuffleTester'] = array();
@@ -14,30 +13,30 @@
     
     // save selected file if in URL
     if (isset($_GET['shuffleFile'])) {
-        $store['loc'] = $root . 'Experiment/' . $_GET['shuffleFile'];
-        $store['get'] = $_GET['shuffleFile'];
+        $store['loc']  = $root . 'Experiment/' . $_GET['shuffleFile'];
+        $store['get']  = $_GET['shuffleFile'];
         $store['name'] = substr($store['loc'], strrpos($store['loc'], '/')+1 );
     }
+    
     // set defaults if values have not been set
     if (!isset($store['loc'])) {
-        $store['loc'] = '';
-        $store['get'] =  '';
+        $store['loc']  = '';
+        $store['get']  = '';
         $store['name'] = '';
     }
     
-    // scan for stimuli files
-    $stimuliFolder   = scandir($root . 'Experiment/Stimuli/');
-    foreach ($stimuliFolder as $item => $path) {
-        if (!inString('.csv', $path, TRUE)) {
-            unset($stimuliFolder[$item]);             // only keep .csv stimuli files
-        }
-    }
-    
-    // scan for procedure files
-    $procedureFolder = scandir($root . 'Experiment/Procedure/');
-    foreach ($procedureFolder as $item => $path) {
-        if (!inString('.csv', $path, TRUE)) {
-            unset($procedureFolder[$item]);             // only keep .csv procedure files
+    // scan for stimuli and procedure files
+    $ShuffleFolders = array(
+        'Stimuli'   => array(),
+        'Procedure' => array()
+    );
+    foreach ($ShuffleFolders as $type => $empty) {
+        $searching = scandir($root . 'Experiment/' . $type . '/');
+        foreach ($searching as $item => $path) {
+            if(!instring('.csv', $path, TRUE)) {
+                unset($searching[$item]);                       // remove files that aren't .csv
+            }
+            $ShuffleFolders[$type] = $searching;
         }
     }
 ?>
@@ -46,6 +45,7 @@
         #shuffleSelectBar {
             width: 100%;
             margin: 0px auto;
+            overflow:auto;
         }
         #shuffleSelectBar * {
             float: left;
@@ -78,65 +78,86 @@
         #oldZoom {
             display:none;
         }
+        #cellContents {
+            width:99%;
+            font-size: .95em;
+            max-width: 99%;
+            margin: 0px auto;
+            height:20px;
+        }
+            .locked {                     /*Added to <textarea> when contents are locked*/
+                border-color: #F0CA31;
+                border-style:dotted;
+                border-width:3%;
+                background-color: #FDF0BE;
+            }
+        .before #RF {
+            float:right;
+        }
     </style>
     
+    <!-- Show bar with dropdown file selector + zoom in/out reset buttons -->
     <div class="toolWidth">
         <div id="shuffleSelectBar">
             <h3>Which file would you like to shuffle?</h3>
+            <!-- Create the select dropdown and populate it with all procedure and stimuli files -->
             <select form="shuffleFile" class="collector-select" name="shuffleFile">
-                <optgroup label="Stimuli Files">
-                    <!-- <option label="$file" value="Stimuli/$file"$selected>$file</option> -->
-                    <?php
-                        foreach ($stimuliFolder as $file) {
-                            if ($store['name'] == $file) {          // add selected property to the file we've shuffled
-                                $selected = ' selected';
-                            } else { $selected = ''; }
-                            echo '<option label="' . $file. '" value="Stimuli/' . $file. '"' . $selected . '>' . $file. '</option>';    //show the option
+            <?php
+                foreach ($ShuffleFolders as $type => $files) {
+                    echo '<optgroup label="' . $type . ' Files">';                  // group options by type: Stimuli OR Procedure
+                    foreach ($files as $file) {
+                        if ($store['get'] == $type . '/' . $file) {                 // marks choosen file as selected
+                            $selected = ' selected';
+                        } else {
+                            $selected = '';
                         }
-                    ?>
-                </optgroup>
-                <optgroup label="Procedure Files">
-                    <!-- <option label="$file" value="Procedure/$file"$selected>$file</option> -->
-                    <?php
-                        foreach ($procedureFolder as $file) {
-                            if ($store['name'] == $file) {          // add selected property to the file we've shuffled
-                                $selected = ' selected';
-                            } else { $selected = ''; }
-                            echo '<option label="' . $file . '" value="Procedure/' . $file . '"' . $selected . '>' . $file . '</option>';   //show the option
-                        }
-                    ?>
-                </optgroup>
+                        // show option for each file readable format--- <option label="$file" value="$type/$file" $selected>  $file  </option>
+                       echo '<option label="' . $file . '" value="' .  $type . '/' . $file . '"' .  $selected . '>' . $file . '</option>';
+                    }
+                }
+            ?>
             </select>
             <button form="shuffleFile">Shuffle!</button>
+            
+            <!-- Show zoom and reset buttons.  Functionality is handled by Jquery at the bottom of the page -->
             <div id="zoom">
                 <button id="in"   >Zoom In</button>
                 <button id="out"  >Zoom Out</button>
                 <button id="reset">Reset </button>
             </div>
         </div>
-        <form id="shuffleFile" action="" method="get"></form>
+    </div>
+    
+    <!-- Show a text area that will be filled with the contents of the cell being hovered over. Functionality is in the JQuery script below -->
+    <div class="toolWidth">
+        <textarea id="cellContents"></textarea>
     </div>
     
 <?php
+    // check if a custom zoom value is in the get
     if (isset($_GET['zoom'])) {
         $val = $_GET['zoom'];
     } else { $val = 'default'; }
-    
-    echo '<input type="text" form="shuffleFile" name="zoom" value="' . $val . '" class="zoomInput" hidden/>';   // save modified zoom value
-    echo '<span id="zoomVal" class="hidden">' . $val . '</span>';                                               // hold any submitted zoom value
-    
-    if ($store['loc'] !== '') {
+?>
+    <input type="text" form="shuffleFile" name="zoom" value="<?= $val ?>" class="zoomInput" hidden/>   <!-- Jquery puts modified zoom value here and it is submitted when shuffle button is pressed-->
+    <span id="zoomVal" class="hidden"><?= $val ?></span>                                               <!-- keeps the zoom value most recently submitted -->
+    <form id="shuffleFile" action="" method="get"></form>                   <!-- the form that submits the selctions made on this page -->
+
+<?php    
+    if ($store['loc'] !== '') {                     // if a shuffle file has been choosen
+        $before = GetFromFile($store['loc']);           // grab file to shuffle
+        $start  = microtime(TRUE);                      // start a timer
+        $after  = multiLevelShuffle($before);           // run basic shuffles
+        $after  = shuffle2dArray($after);               // run advanced shuffles
+        $end    = microtime(TRUE);                      // stop timer
+        $duration = ($end - $start)*1000000;            // calculate timer duration
         
-        $before = GetFromFile($store['loc']);
-        $start  = microtime(TRUE);
-        $after  = multiLevelShuffle($before);
-        $after  = shuffle2dArray($after);
-        $end    = microtime(TRUE);
-        $duration = ($end - $start)*1000000;
-        echo '<div class="before"><h3>Before</h3>';
+        // show the before shuffling version
+        echo '<div class="before"><div id="RF"><h2>Before</h2>';
                   display2dArray($before);
-        echo '</div>';
-        echo '<div class="after"><h3>After</h3>';
+        echo '</div></div>';
+        // show the after shuffling version
+        echo '<div class="after"><h2>After</h2>';
                   display2dArray($after);
         echo '</div>';
     }
@@ -147,31 +168,56 @@
         <dd><code><?= $store['name'] ?></code></dd>
         <dt>File location</dt>
         <dd><code><?= $store['loc']  ?></code></dd>
-        <dt>Time to shuffle</dt>
-        <dd><?=  $duration //round($duration, 5) ?> microseconds</dd>
+<?php
+        if (isset($duration)) {                         // only show duration when a file was actually shuffled
+            echo  '<dt>Time to shuffle</dt>'
+                . '<dd>' . $duration . ' microseconds</dd>';
+        }
+?>
     </dl>
     
 <script type="text/javascript">
-    var iniitalSize = parseInt( $(".display2dArray").css("font-size") );
+    var iniitalSize = parseFloat( $(".display2dArray").css("font-size") );      // save default table font size
     var size = iniitalSize;
-    var zoom = parseInt($("#zoomVal").html());
-    $(".display2dArray").css("font-size", zoom);
+    var zoom = parseFloat($("#zoomVal").html());                                // save custom zoom value
+    var clicked = 0;                                                            // reset click binary (0 = not locked, 1 = locked contents)
+    $(".display2dArray").css("font-size", zoom);                                // change table zoom to custom zoom
 
     $(window).ready(function () {
-        $('#in').click(function (){
-           size = size + 2;
-           $(".display2dArray").css("font-size", size);
-           $(".zoomInput").val(size);
+        $('#in').click(function (){                         // when zoom in button is clicked
+           size = size + 1;                                     // increment the size
+           $(".display2dArray").css("font-size", size);         // change font to new size value
+           $(".zoomInput").val(size);                           // put new zoom value into hidden input
         });
-        $('#out').click(function (){
-           size = size - 2;
-           $(".display2dArray").css("font-size", size);
-           $(".zoomInput").val(size);
+        $('#out').click(function (){                        // when zoom out button is clicked
+           size = size - 1;                                     // decrement the size
+           $(".display2dArray").css("font-size", size);         // change font to the new size value
+           $(".zoomInput").val(size);                           // put new zoom value into hidden input
         });
-        $('#reset').click(function (){
-           size = iniitalSize;
-           $(".display2dArray").css("font-size", size);
-           $(".zoomInput").val(size);
+        $('#reset').click(function (){                      // when reset button is pressed
+           size = iniitalSize;                                  // change size to inititial size
+           $(".display2dArray").css("font-size", size);         // change table font-size back to original
+           $(".zoomInput").val(size);                           // put the original size back into the hidden zoom input field
+           clicked = 0;                                         // set clicked to FALSE / unclicked / unlocked
+           $("textarea").removeClass("locked");                 // remove textarea locked styling
         });
+        $("td div").click(function() {                      // if a table cell is clicked
+            if (clicked == 0) {                                 // if not locked
+                clicked = 1;                                        // lock
+                $("textarea").addClass("locked");                   // add lock style to textarea
+                $("#reset").html("Reset/Unlock");                   // change reset button to reset/unlock button
+            } else {                                            // if locked
+                clicked = 0;                                        // unlock
+                $("textarea").removeClass("locked");                // remove locked styling from textarea
+                $("#reset").html("Reset");                          // change reset button back to "reset"
+            }
+        });
+        $("td div").hover(function() {                      // if you hover over a table cell
+            if (clicked == 0) {                                 // if contents are not locked
+                var contents = $(this).html();                  // save contents of cell being hovered over
+                $("#cellContents").html(contents).val();        // insert contents into textarea in a way that preserves HTML markup
+            }
+        });
+        
     });
 </script>
