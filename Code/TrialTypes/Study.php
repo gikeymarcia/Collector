4fdbcddf5631b61f942417420741ae3c67b378e9
Mikey Garcia <gikeymarcia@gmail.com>
2015-05-06 20:59:03 -0700

    fixed "pop" when Instructions.php animates
    removed helvetica from font-family list
    general css goodness

--- Code/TrialTypes/Study.php
+++ Code/TrialTypes/Study.php
@@ -1,16 +1,14 @@
 <?php
     $compTime = 5;    // time in seconds to use for 'computer' timing
 ?>
-<section class="vcenter precache">
-  <div><?php echo isset($text) ? $text : ""; ?></div>
-  <div class='study clearfix'>
+<div><?php echo isset($text) ? $text : ""; ?></div>
+<div class='study clearfix'>
     <span class="study-left">  <?php echo $cue; ?>    </span>
     <span class="study-divider">         :            </span>
     <span class="study-right"> <?php echo $answer; ?> </span>
-  </div>
-
-  <!-- include form to collect RT and advance page -->
-  <div class="textcenter">
-    <input class="collector-button collector-button-advance" id="FormSubmitButton" type="submit" value="Next" autofocus="">
-  </div>
-</section>
+</div>
+<!-- include form to collect RT and advance page -->
+<div class="textcenter">
+    <button class="collector-button collector-button-advance" 
+    id="FormSubmitButton" type="submit" autofocus=""> Next </button>
+</div>
