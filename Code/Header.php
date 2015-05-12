4288fb81cd4f9985223f91b07f8032545209caa0
gikeymarcia <gikeymarcia@gmail.com>
2015-05-12 04:19:41 -0700

    Added a favicon

--- Code/Header.php
+++ Code/Header.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="utf-8" />
+  <link rel="icon" href="<?= $_rootF . $codeF ?>favicon.png" type="image/png">
+  <link rel="shortcut icon" href="<?= $_rootF . $codeF ?>favicon.png" type="image/png">
   <link rel="stylesheet" href="<?php echo $_codeF . 'css/global.css' ?>">
   <link rel="stylesheet" href="<?php echo $_codeF . 'css/jquery-ui-1.10.4.custom.min.css' ?>">
   <script src="<?php echo $_codeF . 'javascript/jquery-1.10.2.min.js' ?>"></script>
