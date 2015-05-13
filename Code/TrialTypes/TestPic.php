d0c51d647f2979854d2ed8f861efb3a5a9fc8a25
gikeymarcia <gikeymarcia@gmail.com>
2015-05-13 04:02:37 -0700

    fix: TestPic missing autocomplete="off"
    add: trial type-- PicFeedbackCopy

--- Code/TrialTypes/TestPic.php
+++ Code/TrialTypes/TestPic.php
@@ -5,7 +5,7 @@
   <div class="precache pic"><?php echo show($cue); ?></div>
   <div><?php echo $text; ?></div>
   <div class="collector-form-element precache textcenter">
-    <input class="testPic" name="Response" id="Response" type="text" value="">
-    <input class="collector-button"  id="FormSubmitButton" type="submit" value="Submit">
+    <input class="testPic" name="Response" id="Response" type="text" value="" autocomplete="off">
+    <input class="collector-button"  id="FormSubmitButton" type="submit" value="Submit" >
   </div>
 </section>
