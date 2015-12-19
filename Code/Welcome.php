18272c8b2c2e1e2cf0297aa7643be364ae07c7ca
Adam Blake <adamblake@g.ucla.edu>
2015-12-19 15:27:51 -0800

    Combines Welcome and WelcomeBack and cleans up the code for readability.

--- Code/Welcome.php
+++ Code/Welcome.php
@@ -16,103 +16,144 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-    require 'initiateCollector.php';
-    // load page header
-    $title = 'Experiment Login Page';  
-    require $_PATH->get('Header');
-    $action = $_PATH->get('Login', 'url');
 
+require 'initiateCollector.php';
 
-    $currentExp = explode('/', $_SERVER['SCRIPT_NAME']);    // get something like array(... , "Collector", "Experiments", "Demo", "index.php")
-    $currentExp = $currentExp[count($currentExp)-2];        // take directory name (from above example, take "Demo")
-    
-    $_PATH->setDefault('Current Experiment', $currentExp);
-    $_PATH->setDefault('Data Sub Dir', '');                 // the conditions class will automatically create a login counter dir, and it needs this value
-    
-    $_SETTINGS = getCollectorSettings();
-    
-    
-    #### login objects
-    $errors = new ErrorController();
+// determine whether participant is returning or not (see Return.php)
+$isReturningUser = isset($isReturningUser) ? $isReturningUser : false;
 
-    $cond = new ConditionController(
-        $_PATH->get('Conditions'),
-        $_PATH->get('Counter'),
-        $errors,
-        $_SETTINGS->hide_flagged_conditions
-    );
-    
-    $cond->checkConditionsFile($_PATH->get('Procedure Dir'), $_PATH->get('Stimuli Dir'));
-    if ($errors->arePresent()) {
-        $errors->printErrors();
-        exit;
+// get the directory of the currently running script (should be Experiment name)
+$scriptName = filter_input(INPUT_SERVER, 'SCRIPT_NAME', FILTER_SANITIZE_STRING);
+$pathParts = explode('/', $scriptName);
+$currentExp = $pathParts[count($pathParts) - 2];
+
+// Set default path for the Current Experiment
+$_PATH->setDefault('Current Experiment', $currentExp);
+
+// Set default path for the Data directory
+// the conditions class will automatically create a login counter dir, and it needs this value
+$_PATH->setDefault('Data Sub Dir', '');
+
+// load other settings from the appropriate configuration files
+$_SETTINGS = getCollectorSettings();
+
+/*
+ * Login objects
+ */
+$errors = new ErrorController();
+
+$cond = new ConditionController(
+    $_PATH->get('Conditions'),
+    $_PATH->get('Counter'),
+    $errors,
+    $_SETTINGS->hide_flagged_conditions
+);
+
+$cond->checkConditionsFile($_PATH->get('Procedure Dir'), $_PATH->get('Stimuli Dir'));
+if ($errors->arePresent()) {
+    $errors->printErrors();
+    exit;
+}
+$conditions = $cond->getAllConditions();
+
+/*
+ * Display
+ */
+// load page header
+$title = 'Experiment Login Page';
+require $_PATH->get('Header');
+$action = $_PATH->get('Login', 'url');
+
+// modify condition option tag attributes according to Settings
+$options = array();
+foreach ($conditions as $i => $cond) {
+    // default option tag attributes
+    $option = array('value' => $i, 'title' => '', 'style' => '', 'name' => $i + 1);
+
+    // Use condition name if set and names are turned on, else number
+    if ($_SETTINGS->use_condition_names) {
+        $option['name'] = $cond['Description'];
     }
-    $conditions = $cond->getAllConditions();
+
+    // show Stimuli + Procedure files for each condition if set
+    if ($_SETTINGS->show_condition_info) {
+        $option['title'] = "{$cond['Stimuli']} - {$cond['Procedure']}";
+    }
+
+    // change style to greyed out if condition is flagged
+    if (substr($cond['Description'], 0, 1) === '#') {
+        $option['style'] = 'color: grey;';
+    }
+
+    $options[$i] = $option;
+}
+
+// set the login prompt text based on whether this is a returning user or not
+$loginText = "Please enter your {$_SETTINGS->ask_for_login}";
+if ($isReturningUser) {
+    $loginText .= ' and make sure it is the same one you used last time';
+}
+
+// only show the select box if enabled and this is not a returning user
+$selectClass = 'hidden';
+if ($_SETTINGS->show_condition_selector == true && !$isReturningUser) {
+    $selectClass = 'collectorInput';
+}
+
 ?>
+
 <!-- Page specific styling tweaks -->
 <style>
-    #indexLogin {
-        margin-top: 2em;
-    }
-    #indexLogin div:first-of-type{
-        margin-bottom: .5em;
-    }
-    #indexLogin input[type="text"] {
-        width: 250px;
-    }
-    #indexLogin  select {
-        width: 150px;
-    }
+  #indexLogin {
+    margin-top: 2em;
+  }
+  #indexLogin div:first-of-type{
+    margin-bottom: .5em;
+  }
+  #indexLogin input[type="text"] {
+    width: 250px;
+  }
+  #indexLogin  select {
+    width: 150px;
+  }
 </style>
 
-<form   id="content"            name="Login"
-        action="<?=$action?>"   method="get"
-        autocomplete="off"      class="index"   >
-    <h1 class="textcenter"><?= $_SETTINGS->welcome ?></h1>
-    <?= $_SETTINGS->exp_description ?>
+<form id="content"             name="Login"
+      action="<?= $action ?>"  method="get"
+      autocomplete="off"       class="index" >
+  <h1 class="textcenter"><?= $_SETTINGS->welcome ?></h1>
+  <?= $_SETTINGS->exp_description ?>
     
-    <section id="indexLogin" class="flexVert">
-        <div class="textcenter flexChild">
-            <?= "Please enter your $_SETTINGS->ask_for_login"  ?>
-        </div>
-        <div class="flexChild">
-            <input name="Username" type="text" value="" class="collectorInput" placeholder="<?= $_SETTINGS->ask_for_login ?>">
-            
-            <!-- Condition selector -->
-        <?php if ($_SETTINGS->show_condition_selector == true): ?>
-            <select name="Condition" class="collectorInput">
-        <?php else: ?>
-            <select class="hidden" name="Condition">
-        <?php endif; ?>
-                <option default selected value="Auto">Auto</option>
-        <?php  // Display conditions as options
-                foreach ($conditions as $i => $cond) {
-                    if ($_SETTINGS->use_condition_names) {
-                        $name = $cond['Description'];
-                    } else {
-                        $name = $i+1;
-                    }
-                    // showing Stimuli + Procedure files for each condition
-                    if ($_SETTINGS->show_condition_info) {
-                        $title = "title='{$cond['Stimuli']} - {$cond['Procedure']}'";
-                    } else {
-                        $title = '';
-                    }
-                    // make flagged conditions grey
-                    if (substr($cond['Description'],0,1) === '#') {
-                        $style = 'style="color: grey;"';
-                    } else {
-                        $style = '';
-                    }
-                    // put this condition in the dropdown selector
-                    echo "<option value='$i' $title $style>$name</option>";
-                }
-        ?>
-            </select>
-            <input type="hidden" name="CurrentExp" value="<?= $currentExp ?>">
-            <button class="collectorButton" type="submit">Login</button>
-        </div>
-    </section>
+  <section id="indexLogin" class="flexVert">
+    <div class="textcenter flexChild">
+      <?= $loginText ?>
+    </div>
+  
+    <div class="flexChild">
+      <input name="Username" type="text" value="" class="collectorInput" placeholder="<?= $_SETTINGS->ask_for_login ?>">
+
+      <!-- Condition selector -->
+      <select name="Condition" class="<?= $selectClass ?>">
+        <option default selected value="Auto">Auto</option>
+        
+        <?php foreach ($options as $o): ?>
+        <option value="<?= $o['value'] ?>" title="<?= $o['title'] ?>" style="<?= $o['style'] ?>">
+            <?= $o['name'] ?>
+        </option>
+        <?php endforeach; ?>  
+      
+      </select>
+      
+      <!-- Submit button -->
+      <?php if ($isReturningUser): ?>
+      <input type="hidden" name="returning" value="1">
+      <?php endif; ?>
+      <input type="hidden" name="CurrentExp" value="<?= $currentExp ?>">
+      <button class="collectorButton" type="submit">Login</button>
+      
+    </div>
+  </section>
 </form>
+
 <?php 
     require $_PATH->get('Footer');
