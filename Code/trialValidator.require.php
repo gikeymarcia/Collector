c56436b62c69c6424dbaebca3fecfdc864d95c31
Adam Blake <adamblake@g.ucla.edu>
2015-12-20 14:14:35 -0800

    phpcs fixes for trialValidator.require.php

--- Code/trialValidator.require.php
+++ Code/trialValidator.require.php
@@ -1,45 +1,44 @@
 <?php
-    // determine if we need to run the trial validator
-    $doTrialValidation = true;
-    
-    // find the mod time of all the files for this experiment
-    clearstatcache();
-    
-    $allExpFiles = array_merge(
-        scanDirRecursively($_PATH->get('Current Experiment')),
-        scanDirRecursively($_PATH->get('Common'))
-    );
-    
-    $fileModTimes = array();
-    
-    foreach ($allExpFiles as $file) {
-        $fileModTimes[] = filemtime($file);
+// determine if we need to run the trial validator
+$doTrialValidation = true;
+
+// find the mod time of all the files for this experiment
+clearstatcache();
+
+$allExpFiles = array_merge(
+    scanDirRecursively($_PATH->get('Current Experiment')),
+    scanDirRecursively($_PATH->get('Common'))
+);
+
+$fileModTimes = array();
+
+foreach ($allExpFiles as $file) {
+    $fileModTimes[] = filemtime($file);
+}
+
+$fileModTimesString = implode(',', $fileModTimes);
+
+// compare against the recorded mod time of the last successful validation
+$trialValidationTimeFile = $_PATH->get('Trial Validation Scan Time');
+
+if (is_file($trialValidationTimeFile)) {
+    $trialValidationTime = file_get_contents($trialValidationTimeFile);
+
+    if ($trialValidationTime === $fileModTimesString) {
+        $doTrialValidation = false;
     }
-    
-    $fileModTimes = implode(',', $fileModTimes);
-    
-    // compare the mod time against the recorded mod time of the last successful validation
-    $trialValidationTimeFile = $_PATH->get('Trial Validation Scan Time');
-    
-    if (is_file($trialValidationTimeFile)) {
-        $trialValidationTime = file_get_contents($trialValidationTimeFile);
-        
-        if ($trialValidationTime === $fileModTimes) {
-            $doTrialValidation = false;
+}
+
+// if we need to validate, use the validation class
+// if successful, record the mod times of the current scan
+if ($doTrialValidation) {
+    $trialValidator = new TrialValidator($stimuli, $procedure, $errors);
+
+    if ($trialValidator->isValid()) {
+        $dir = dirname($trialValidationTimeFile);
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
         }
+        file_put_contents($trialValidationTimeFile, $fileModTimesString);
     }
-    
-    // if we need to validate, use the validation class
-    // if successful, record the mod times of the current scan
-    if ($doTrialValidation) {
-        
-        $trialValidator = new TrialValidator($stimuli, $procedure, $errors);
-        
-        if ($trialValidator->isValid()) {
-            $dir = dirname($trialValidationTimeFile);
-            if (!is_dir($dir)) {
-                mkdir($dir, 0777, true);
-            }
-            file_put_contents($trialValidationTimeFile, $fileModTimes);
-        }
-    }
+}
