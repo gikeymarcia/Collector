639a19532f13c493e581517ead260330538e6069
gikeymarcia <gikeymarcia@gmail.com>
2016-02-16 23:14:42 -0800

    Added MediaScanner tool that makes a stimuli file based on the contents of your '/Experiments/Common/Media' folder.

---
+++ Tools/MediaScanner/download.php
@@ -0,0 +1,17 @@
+<?php 
+require '../loginFunctions.php';
+adminOnly();
+
+$file = 'tempStim.csv';
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
