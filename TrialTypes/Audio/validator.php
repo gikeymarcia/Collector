49b923b4aea4498428f043fd2c85826bc54ac5d8
gikeymarcia <gikeymarcia@gmail.com>
2016-03-17 23:01:48 -0700

    Audio trial type correctly documented:
    -validator written
    -demo stimuli file
    -demo procedure file
    -modified demo Conditions.csv to include my trial demo
    -PNG capture of trial running in full screen
    
    To-do: figure how why commented bit of validator was not working as I'd planned

---
+++ TrialTypes/Audio/validator.php
@@ -0,0 +1,15 @@
+<?php
+return function($trialValues) {
+    $errors = array();
+    if (!isset($trialValues['Cue'])) {
+        $errors[] = "Your 'Audio' trial tyle needs a 'Cue' value.";
+    }
+    // if (show($trialValues['Cue']) == $trialValues['Cue']) {
+    //     $errors[] = "Your 'Audio' trial type's 'Cue' value does not point to a " .
+    //                 "filename with an extension. For example,'Mikey.jpg'. Your " .
+    //                 "'Cue' is: {$trialValues['Cue']}";
+    // }
+    return $errors;
+};
+
+?>
