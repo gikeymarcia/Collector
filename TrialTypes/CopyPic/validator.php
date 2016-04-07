8a1e117639a67c01854e1f4360af750ad63dd8c5
michaelevans18 <michaelevans18@g.ucla.edu>
2016-04-07 08:22:32 -0700

    Added validator file to CopyPic, changed a type in Audio validator

--- TrialTypes/CopyPic/validator.php
+++ TrialTypes/CopyPic/validator.php
@@ -2,14 +2,14 @@
 return function($trialValues) {
     $errors = array();
     if (!isset($trialValues['Cue'])) {
-        $errors[] = "Your 'Audio' trial type needs a 'Cue' value.";
+        $errors[] = "Your 'CopyPic' trial type needs a 'Cue' value.";
     }
     if (show($trialValues['Cue']) == $trialValues['Cue']) {
-        $errors[] = "Your 'Audio' trial type's 'Cue' value does not point to a " .
-                    "filename with an extension. For example,'Crystal Glass.mp3'. Your " .
+        $errors[] = "Your 'CopyPic' trial type's 'Cue' value does not point to a " .
+                    "filename with an extension. For example,'picture.jpg'. Your " .
                     "'Cue' is: {$trialValues['Cue']}";
     }
     return $errors;
 };
-
+​
 ?>
