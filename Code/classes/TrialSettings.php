cc6a3ff2b1c914904117829ef76321001bc19ee2
Adam Blake <adamblake@g.ucla.edu>
2016-05-20 18:10:49 -0700

    Simplifies and refactors parseSettings

--- Code/classes/TrialSettings.php
+++ Code/classes/TrialSettings.php
@@ -7,6 +7,11 @@ namespace Collector;
 
 /**
  * @todo add description for TrialSettings
+ * 
+ * All settings must be of the format [key] = [value]. Settings must be
+ * separated by the "|" character (e.g. "setting1=a|setting2=b"). A setting
+ * can be assigned an array of values through the following syntax:
+ * arraySetting[]=1;2;3;4, which will yield arraySetting = array(1, 2, 3, 4).
  */
 class TrialSettings
 {
@@ -51,16 +56,17 @@ class TrialSettings
     }
     
     /**
-     * Parses the raw settings string.
+     * Parses the raw settings string and sets the values to 'settings'.
      */
     protected function parseSettings() {
         $settings = array();
-        $rawSettings = $this->rawSettings;
         
-        $splitRawSettings = explode('|', $rawSettings);
-        foreach ($splitRawSettings as $rawSetting) {
+        foreach (explode('|', $this->rawSettings) as $rawSetting) {
             $splitSetting = explode('=', $rawSetting);
             $key = strtolower(trim($splitSetting[0]));
+            $val = isset($splitSetting[1]) ? trim($splitSetting[1]) : null;
+            
+            // determine if setting should be parsed as an array
             if (substr($key, -2) === '[]') {
                 $key = substr($key, 0, -2);
                 $getValuesAsArray = true;
@@ -68,38 +74,38 @@ class TrialSettings
                 $getValuesAsArray = false;
             }
             
-            if ($key === '') {
-                // can't have empty key, makes no sense
+            // do not overwrite, and skip invalid keys
+            if (isset($settings[$key]) || empty($key)) {
                 continue;
             }
             
-            if (isset($settings[$key])) {
-                // dont overwrite
-                continue;
-            }
-                
-            if (!isset($splitSetting[1])) {
-                if ($getValuesAsArray) {
-                    $settings[$key] = array();
-                } else {
-                    $settings[$key] = true;
-                }
-            } else {
-                if ($getValuesAsArray) {
-                    $valArray = array();
-                    $valSplit = explode(';', $splitSetting[1]);
-                    foreach ($valSplit as $subVal) {
-                        $subVal = trim($subVal);
-                        if ($subVal !== '') $valArray[] = $subVal;
-                    }
-                    $settings[$key] = $valArray;
-                } else {
-                    $settings[$key] = trim($splitSetting[1]);
-                }
-            }
+            // set the values
+            $settings[$key] = $getValuesAsArray
+                ? (isset($val) ? $this->parseArraySetting($val) : array())
+                : (isset($val) ? $val : true);
         }
         
         $this->settings = $settings;
+    }
+    
+    /**
+     * Converts a string of values separated by semicolons to an array.
+     * 
+     * @param string $value The string to convert.
+     * 
+     * @return array Returns the array of values with whitespace trimmed.
+     */
+    protected function parseArraySetting($value)
+    {
+        $valArray = array();
+        foreach (explode(';', $value) as $subVal) {
+            $subVal = trim($subVal);
+            if (!empty($subVal)) {
+                $valArray[] = $subVal;
+            }
+        }
+        
+        return $valArray;
     }
     
     /**
