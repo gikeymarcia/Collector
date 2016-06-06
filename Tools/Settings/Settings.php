359a7bd9eff55bab0b1b84c0cba51db8c37c44b2
gikeymarcia <gikeymarcia@gmail.com>
2016-06-06 15:09:29 -0700

    smoother animation
    expander buttons for Settings

--- Tools/Settings/Settings.php
+++ Tools/Settings/Settings.php
@@ -50,12 +50,12 @@ require 'makeSettingOptions.php';
     if (!empty($_DATA['exp'])) {
         $name = $_DATA['exp'];
         echo '<div class="exp settings toolWidth">';
-            echo "<h3 class='type'>Experiment Settings ($name)</h3>";
+            echo "<h3 class='type'>Experiment Settings ($name)<button type='button' class='expander'>&#8679;</button></h3>";
             setting_string($yourSettings, "experiment_name" ,  "Experiment Name", $tooltips);
             setting_bool  ($yourSettings, "debug_mode"      ,  "Debug Mode"     , $tooltips);
             setting_string($yourSettings, "lenient_criteria", "Lenient Criteria", $tooltips);
 
-            echo "<div class='subgroup'><h4>Welcome Screen Settings</h4>";
+            echo "<div class='subgroup'><h4>Welcome Screen Settings<button type='button' class='expander'>&#8679;</button></h4>";
             setting_string($yourSettings, "welcome"                , "Welcome Message"        , $tooltips);
             setting_string($yourSettings, "exp_description"        , "Welcome Description"    , $tooltips);
             setting_string($yourSettings, "ask_for_login"          , "Asked for Login"        , $tooltips);
@@ -65,7 +65,7 @@ require 'makeSettingOptions.php';
             setting_bool  ($yourSettings, "hide_flagged_conditions", "Hide Flagged Conditions", $tooltips);
             echo "</div>";
 
-            echo "<div class='subgroup'><h4>Done Screen</h4>";
+            echo "<div class='subgroup'><h4>Done Screen<button type='button' class='expander'>&#8679;</button></h4>";
             setting_string($yourSettings, "verification", "Verification Code", $tooltips);
             echo "</div>";
 
@@ -75,7 +75,7 @@ require 'makeSettingOptions.php';
 
 <!-- This is where you edit common experiment settings -->
 <div class="common settings toolWidth">
-    <h3 class='type'>Common Settings (Shared Across Experiments)</h3>
+    <h3 class='type'>Common Settings (Shared Across Experiments)<button type='button' class='expander'>&#8679;</button></h3>
     <?php
         // echo ""
         setting_string($yourSettings, "experimenter_email" , "Experimenter Email" , $tooltips);
@@ -94,7 +94,7 @@ require 'makeSettingOptions.php';
 
 <script type="text/javascript">
     // hide the default option when showing the dropdown box
-    $(".expSelect select").focusin(function(event) {
+    $(".expSelect select").focusin(function() {
         $("#selectLabel").css("display","none");
     });
 
@@ -103,12 +103,27 @@ require 'makeSettingOptions.php';
         $("#chooseExp").submit();
     });
 
+    var up   = $("<button>").html("&#8679;").html();
+    var down = $("<button>").html("&#8681;").html();
+
     // click header to toggle experiment/common settings
     $(".type").click(function() {
-        $(this).siblings().toggle(350);
+        $(this).siblings().toggle(450);
+        accordianIcon = $(this).children("button");
+        if (accordianIcon.html() == up) {
+            accordianIcon.html("&#8681;");
+        } else {
+            accordianIcon.html("&#8679;");
+        }
     });
     // click subheading to toggle a group of settings
-    $(".subgroup h4").click(function(event) {
-        $(this).siblings().toggle(350);
+    $(".subgroup h4").click(function() {
+        $(this).siblings().toggle(450);
+        accordianIcon = $(this).children("button");
+        if (accordianIcon.html() == up) {
+            accordianIcon.html("&#8681;");
+        } else {
+            accordianIcon.html("&#8679;");
+        }
     });
 </script>
