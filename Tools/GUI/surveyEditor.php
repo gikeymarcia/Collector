a3a3c1b74a1777b13c60358b81fe23a5fea0f1ad
AnthonyHaffey <anthony.haffey@gmail.com>
2016-07-12 13:49:33 +0100

    Developed helper bar - still missing advice for settings. More work needed to stop user (deliberately) breaking it. But usable for users not trying to break.

--- Tools/GUI/surveyEditor.php
+++ Tools/GUI/surveyEditor.php
@@ -36,7 +36,7 @@
     margin: 10px 0 40px; 
   }
   form {
-    text-align: center;
+    text-align: left;
   }
   .tableArea {
     display: block;
@@ -73,13 +73,19 @@
     box-sizing: border-box;
     padding: 10px;
     vertical-align: top;
-    margin-top: 60px;
+    margin-top: 180px;
   }
   
   #TableForm {
     display: inline-block;
     width: 75%;
     box-sizing: border-box;
+  }
+  .typeHeader{
+    color:blue;
+  }
+  .typeHeader:hover{
+    color:green ;
   }
 </style>
 
@@ -200,7 +206,7 @@
 
 <form action='index.php' method='post' id="TableForm">
   <textarea id="currentGuiSheetPage" name="currentGuiSheetPage" style="display:none">surveyEditor</textarea>
-  <h1>
+  <h1 style="padding:30px">
     <?php 
       if(strcmp($surveySheetsInfo->thisSurveyName,"[No survey Selected]")==0){
         ?>
@@ -211,11 +217,10 @@
         } else { ?>
           <textarea id="currSurveyName" name="currSurveyName" style="color:#069;" rows="1" 
           onchange="checkName()"><?=$surveySheetsInfo->thisSurveyName?></textarea>        
-        <?php } ?>
-    
-    
+        <?php } ?>  
   </h1>
-  <span>
+  
+  <span style="padding:30px">
     <button name="newSurvey" value="stim" class="collectorButton" id="newSurveyButton"> new survey </button>
 
     <select name="surveyPostName" title="[filename]">
@@ -230,8 +235,15 @@
     ?>    
     </select>
     <button type='submit' name="openButton" class='collectorButton' value='Select'>Open</button>
+    <input id="saveButton" type="button" class="collectorButton" value="Save">  
+    <button id="submitButton" type="submit" name="Save" class="collectorButton" style="display:none"></button> 
+    <input type="button" id="deleteSheetButton" name="DeleteSheetQuestion" class="collectorButton" value="Delete?">  
+    <button id="deleteActivate" type="submit" name="DeleteSheet" class="collectorButton" value="Delete" style="display:none">No text needed</button>
+    <input id="helperButton" type="button" class="collectorButton" value="Hide Helper"> 
+  
+    
   </span>
-
+  
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
   <link rel="stylesheet" href="GUI/handsontables/handsontables.full.css">
   <script src="GUI/handsontables/handsontables.full.js"></script>
@@ -250,43 +262,78 @@
   <br>
 
   <input type="hidden" name="stimTableInput">
-    
-  <input id="saveButton" type="button" class="collectorButton" value="Save">  
-  <button id="submitButton" type="submit" name="Save" class="collectorButton" style="display:none"></button> 
-
-
-  
-  <input type="button" id="deleteSheetButton" name="DeleteSheetQuestion" class="collectorButton" value="Delete?">  
-  <button id="deleteActivate" type="submit" name="DeleteSheet" class="collectorButton" value="Delete" style="display:none">No text needed</button>
-      
   
 </form>
 
 <!-- the helper bar !-->
+  
 <div id="helperBar">
-  <input type="button" class="collectorButton" value="minimise"> 
   <h1> Helper </h1>
   <h2 id="helpType">Select Cell</h2>
   
   <!-- Help Types -->
   <div class="helpType_Col" id="helpType_Answers">
-    Here is some more information about Answers.
+    Answers can take two forms. If you want a range of numbers, you can type:<br><br>
+    <em>FirstNumber::LastNumber</em>
+    <br><br><br>
+    If you have a list of values you want the participant to choose from, you can list them with a pipe (|) in the middle. E.g.: <br><br>
+    <em>Cat|Dog|Bird</em>
+    
+    <br><br><br>
+    Alternatively, you can just leave this value empty if the participant isn't making a choice. For example, for <b>Text</b> or <b> Date</b> questions there are answers to choose from.
   </div>
   
-  <div class="helpType_Col" id="helpType_QuestionName">
-    Here is some more information about Question Names.
+  <div class="helpType_Col" id="helpType_Values">
+    <b>Values</b> are stored for each answer given. This is useful if you want to have a score associated with each answer. You need the same number of <b>Values</b> as you have of <b>Answers</b>. This is because <b>Answers need to map onto Values</b>.
+    
+    <br><br><br>    
+    As a result, like <b>Answers</B>, values can take two forms.
+     
+    If you want a range of numbers, you can type:<br><br>
+    <em>FirstNumber::LastNumber</em>
+    <br><br><br>
+    If you have a list of values you want the participant to choose from, you can list them with a pipe (|) in the middle. E.g.: <br><br>
+    <em>Cat|Dog|Bird</em>
+    
+    <br><br><br>
+    Alternatively, you can just leave this value empty if the participant isn't making a choice. For example, for <b>Text</b> or <b> Date</b> questions there are answers to choose from.
   </div>
+  
+  <div class="helpType_Col" id="helpType_Question">
+    Just type in the question in this column.
+  </div>
+
+  <div class="helpType_Col" id="helpType_QuestionName">
+    Question names are used for storing the data, so each question name has to be unique. Question names are not required for page Breaks.    
+  </div>
+  
+  <div class="helpType_Col" id="helpType_Required">
+    Identify in this column whether or not the participant needs to complete this question to proceed. The default assumption is that they do not, but if you want to force a participant to respond to a question, type in "Yes".
+  </div>
+
+  <div class="helpType_Col" id="helpType_Shuffle">
+    <b>Shuffling</b> is used to randomise the order of stimuli. The default assumption is that there is no shuffling. However, if you want to randomise the order of certain rows, then type in the same value for each of the rows you want shuffled. For example, if I want rows 1,3 and 5 shuffled, but not rows 2 and 4, I could put in these values in this column:
+    <br><br>
+    shuffle135<br>
+    No<br>
+    shuffle135<br>
+    No<br>
+    shuffle135
+  </div>
+  
 
   <div class="helpType_Col" id="helpType_Type">
     <?php
-      $surveyTypes=readCsv("GUI/surveyTypes.csv");
-      //var_dump($surveyTypes);
+      $surveyTypes=getFromFile("GUI/surveyTypes.csv",",");
+      $surveyTypes=array_slice($surveyTypes,2);
       
       $surveyTypeVector=[];
       foreach($surveyTypes as $surveyType){
-        array_push($surveyTypeVector,$surveyType[0]);
-        echo "<h3 id='header$surveyType[0]' onclick='hideShow(\"detail$surveyType[0]\")'>$surveyType[0]</h3>";
-        echo "<div id='detail$surveyType[0]' style='display:none'>$surveyType[1]</div>";
+        array_push($surveyTypeVector,$surveyType["surveyType"]);
+        ?>
+        <h3 class="typeHeader" id='header<?=$surveyType["surveyType"]?>' onclick='hideShow("detail<?=$surveyType["surveyType"]?>")'><?=$surveyType["surveyType"]?></h3>
+        <div id='detail<?=$surveyType["surveyType"]?>' style='display:none'><?=$surveyType["surveyDetail"]?></div>
+        <?php
       }
         $jsonSurveyVector=json_encode($surveyTypeVector);
     ?>
@@ -301,6 +348,20 @@
 
 
 <script type="text/javascript">
+
+$("#helperButton").click(function(){
+  if(helperButton.value=="Hide Helper"){
+    helperButton.value="Show Helper";
+    $("#helperBar").hide();
+    TableForm.style.width="100%";//expand width of table
+    updateDimensions(stimTable);
+  } else {
+    helperButton.value="Hide Helper";
+    TableForm.style.width="75%";//expand width of table
+    updateDimensions(stimTable);
+    $("#helperBar").show();
+  }
+});
 
 function hideShow(x){
   if($('#'+x).is(':visible')) {
@@ -468,7 +529,7 @@ var stimTable;
         var thisArea = $(container).closest(".tableArea");
         
         thisWidth  = Math.min(thisWidth,  thisArea.width());
-        thisHeight = Math.min(thisHeight, 600);
+        thisHeight = Math.min(thisHeight, 700);
         
         hot.updateSettings({
             width:  thisWidth,
