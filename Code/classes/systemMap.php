c6b7625b96f8d8a647c81e5b48d976089088d4c2
gikeymarcia <gikeymarcia@gmail.com>
2016-07-30 23:08:57 -0700

    new system map. Sure there are some mistakes for now.

--- Code/classes/systemMap.php
+++ Code/classes/systemMap.php
@@ -1,257 +1,112 @@
 <?php
-/**
- * System Map.
- *
- * To use this file, map out the directory structure of the program, using keys
- * as directory or file names, and values as either the contents of a directory,
- * or the label for that directory or file. Then set a variable equal to a
- * require or include of the file, e.g. $sysMap = require 'systemMap.php';
- *
- * Inside the map, certain keys can be a variable using 'Dir Name' as the key,
- * you can give a label to the directory, using {var}, you can provide a point
- * for the user to provide their own information (such as the specific trial
- * type "instruct").
- *
- * Using {Default Name}, you can create a different type of variable, which
- * should only need to be set once for the experiment good uses for this include
- * the default procedure file or the default output file This can just be part
- * of a file, such as '{Username}.json'
- *
- * More information can be found inside the Pathfinder class.
- *
- * @see Pathfinder
- */
 return array(
-    'Dir Name' => 'root',
-
-    'index.php' => 'index',
-
-    'Experiments' => array(
-        'Dir Name' => 'Experiments',
-
-        '_Common' => array(
-            'Dir Name' => 'Common',
-
-            'Common Settings.json' => 'Common Settings',
-
-            'Password.php' => 'Password',
-
-            'Ineligible' => array(
-                'Dir Name' => 'Ineligibility Dir',
-            ),
-
-            'Media' => array(
-                'Dir Name' => 'Media',
-            ),
-
-            'TrialTypes' => array(
-                'Dir Name' => 'Custom Trial Types',
-
-                '{var}' => array(
-                    'Dir Name' => 'Custom Trial Type Dir',
-
-                    'display.php' => 'Custom Trial Display',
-                    'helper.inc' => 'Custom Trial Helper',
-                    'scoring.php' => 'Custom Trial Scoring',
-                    'script.js' => 'Custom Trial Script',
-                    'style.css' => 'Custom Trial Style',
-                    'validator.php' => 'Custom Trial Validator',
-                ),
-            ),
-        ),
-
-        '{Current Experiment}' => array(
-            'Dir Name' => 'Current Experiment',
-
-            'index.php' => 'Current Index',
-            'Return.php' => 'Current Return',
-
-            'Conditions.csv' => 'Conditions',
-            'Task Instructions.php' => 'Instructions',
-
-            'Settings.json' => 'Experiment Settings',
-
-            'Stimuli' => array(
-                'Dir Name' => 'Stimuli Dir',
-
-                '{Stimuli}' => 'Stimuli',
-            ),
-
-            'Procedure' => array(
-                'Dir Name' => 'Procedure Dir',
-
-                '{Procedure}' => 'Procedure',
-            ),
-        ),
-    ),
-
-    'Code' => array(
-        'Dir Name' => 'Code',
-
-        'Welcome.php' => 'Welcome',
-        'WelcomeBack.php' => 'Welcome Back',
-
-        'check.php' => 'Check',
-
-        'customFunctions.php' => 'Custom Functions',
-
-        'Done.php' => 'Done',
-
-        'errorCheck.php' => 'Error Check',
-
-        'Experiment.php' => 'Experiment Page',
-        'experiment.require.php' => 'Experiment Require',
-
-        'footer.php' => 'Footer',
-
-        'Header.php' => 'Header',
-
-        'icon.png' => 'Icon',
-
-        'initiateCollector' => 'Initiate Collector',
-
-        'instructions.php' => 'Instructions Page',
-
-        'instructionsRecord.php' => 'Instructions Record',
-
-        'login.php' => 'Login',
-
-        'trialValidator.require.php' => 'Trial Validator Require',
-
-        'nojs.php' => 'No JS',
-
-        'setPassword.php' => 'Set Password',
-
-        'shuffleFunctions.php' => 'Shuffle Functions',
-        'customFunctions.php' => 'Custom Functions',
-
-        'css' => array(
-            'Dir Name' => 'CSS Dir',
-
-            'global.css' => 'Global CSS',
-            'jquery-ui.min.css' => 'Jquery UI CSS',
-            'jquery-ui-1.10.4.custom.min.css' => 'Jquery UI Custom CSS',
-
-            'nojswarning.png' => 'No JS Warning',
-
-            'tools.css' => 'Tools CSS',
-            'tutorial.css' => 'Tutorial CSS',
-
-            'fonts' => array(
-                'Dir Name' => 'Fonts',
-
-                // wow there is a lot in here. . .
-            ),
-
-            'images' => array(
-                'Dir Name' => 'CSS Images',
-
-                // lots of stuff in here
-            ),
-        ),
-
-        'classes' => array(
-            'Dir Name' => 'Classes',
-
-            'ConditionController.php' => 'Conditions Class',
-            'ControlFile.php' => 'Control Files Class',
-            'DebugController.php' => 'Debug Class',
-            'ErrorController' => 'Errors Class',
-            'Parse.php' => 'Parse',
-            'Pathfinder.php' => 'Pathfinder',
-            'Procedure.php' => 'Procedure Class',
-            'ReturnVisitController.php' => 'Return Class',
-            'SideData.php' => 'SideData Class',
-            'StatusController.php' => 'Status Class',
-            'Stimuli.php' => 'Stimuli Class',
-            'systemMap.php' => 'system map',
-            'TrialValidator.php' => 'Trial Validator Class',
-            'User.php' => 'Users Class',
-        ),
-
-        'javascript' => array(
-            'Dir Name' => 'JS Dir',
-
-            'Collector.js' => 'Collector JS',
-
-            'jquery.js' => 'Jquery',
-            'jquery-ui-1.10.4.custom.min.js' => 'Jquery UI Custom',
-            'jquery-1.12.4.min.js' => 'Jquery UI',
-
-            'loggingIn.js' => 'Login JS',
-            'sha256.js' => 'Sha256 JS',
-        ),
-
-        'phpbrowscap' => array(
-            'Browscap.php' => 'Browscap',
-            // more stuff in here
-        ),
-
-    ),
-
-    'Data' => array(
-        'Dir Name' => 'Data',
-
-        '{Current Experiment}-Data{Data Sub Dir}' => array(
-            'Dir Name' => 'Current Data Dir',
-
-            'InstructionsData.csv' => 'Instructions Data',
-            'Status_Begin.csv' => 'Status Begin Data',
-            'Status_End.csv' => 'Status End Data',
-            'SideData.csv' => 'SideData Data',
-
-            'Counter' => array(
-                'Dir Name' => 'Counter Dir',
-
-                'Counter.csv' => 'Counter',
-            ),
-
-            'session' => array(
-                'Dir Name' => 'Session Storage Dir',
-
-//                '{Username}.json' => 'Session Storage',
-                '{Username}.txt' => 'Session Storage',
-            ),
-
-            'Output' => array(
-                'Dir Name' => 'Output Dir',
-
-                '{Output}' => 'Experiment Output',
-            ),
-
-            'validations' => array(
-                'Dir Name' => 'Trial Validations Dir',
-
-                'scanTime-{Condition Index}.txt' => 'Trial Validation Scan Time',
-            ),
-        ),
-    ),
-
-    'Admin' => array(
-        'Dir Name' => 'Admin',
-
-        'index.php' => 'Admin Index',
-        
-        'Tools' => array(
-            'Dir Name' => 'Tools'
-        )
-        // lots of stuff in here
-    ),
-
-    'TrialTypes' => array(
-        'Dir Name' => 'Trial Types',
-
-        '{var}' => array(
-            'Dir Name' => 'Trial Type Dir',
-
-            'display.php' => 'Trial Display',
-            'helper.php' => 'Trial Helper',
-            'scoring.php' => 'Trial Scoring',
-            'script.js' => 'Trial Script',
-            'style.css' => 'Trial Style',
-            'validator.php' => 'Trial Validator',
-        ),
-        'defaultTrialHelper.php' => 'default helper',
-        'defaultTrialScoring.php' => 'default scoring',
-    ),
+'root'                       => array('Dir'  , '.'),
+'index'                      => array('Plain', 'index.php'),
+
+'Experiments'                => array('Dir'  , 'Experiments/'),
+  'Common'                   => array('Dir'  , 'Experiments/Common/'),
+  'Common Settings'          => array('File' , 'Experiments/Common/Common Settings.json'),
+  'Password'                 => array('File' , 'Experiments/Common/Password.php'),
+  'Ineligibility Dir'        => array('Dir'  , 'Experiments/Common/Ineligible/')
+  'Media'                    => array('Dir'  , 'Experiments/Common/Media/')
+  'Custom Trial Types'       => array('Dir'  , 'Experiments/Common/TrialTypes/')
+  'Custom Trial Type Dir'    => array('Dir'  , 'Experiments/Common/TrialTypes/[var]/')
+    'Custom Trial Display'   => array('File' , 'Experiments/Common/TrialTypes/[var]/display.php')
+    'Custom Trial Helper'    => array('File' , 'Experiments/Common/TrialTypes/[var]/helper.inc')
+    'Custom Trial Scoring'   => array('File' , 'Experiments/Common/TrialTypes/[var]/scoring.php')
+    'Custom Trial Script'    => array('File' , 'Experiments/Common/TrialTypes/[var]/script.js')
+    'Custom Trial Style'     => array('File' , 'Experiments/Common/TrialTypes/[var]/style.css')
+    'Custom Trial Validator' => array('File' , 'Experiments/Common/TrialTypes/[var]/validator.php')
+
+  'Current Experiment'       => array('Dir'  , 'Experiments/[Current Experiment]/')
+  'Current Index'            => array('File' , 'Experiments/[Current Experiment]/index.php')
+  'Current Return'           => array('File' , 'Experiments/[Current Experiment]/Return.php')
+  'Conditions'               => array('ioCSV', 'Experiments/[Current Experiment]/Conditions.csv')
+  'Experiment Settings'      => array('File' , 'Experiments/[Current Experiment]/Settings.json')
+  'Stimuli Dir'              => array('Dir'  , 'Experiments/[Current Experiment]/Stimuli/')
+    'Stimuli'                => array('ioCSV', 'Experiments/[Current Experiment]/Stimuli/[Stimuli]')
+  'Procedure Dir'            => array('Dir'  , 'Experiments/[Current Experiment]/Procedure/')
+    'Procedure'              => array('ioCSV', 'Experiments/[Current Experiment]/Procedure/[Procedure]')
+
+'Code'                       => array('Dir'  , 'Code/'),
+  'Welcome'                  => array('File' , 'Code/Welcome.php'),
+  'Welcome Back'             => array('File' , 'Code/WelcomeBack.php'),
+  'Check'                    => array('File' , 'Code/check.php'),
+  'Custom Functions'         => array('File' , 'Code/customFunctions.php'),
+  'Done'                     => array('File' , 'Code/Done.php'),
+  'Error Check'              => array('File' , 'Code/errorCheck.php'),
+  'Experiment Page'          => array('File' , 'Code/Experiment.php'),
+  'Experiment Require'       => array('File' , 'Code/experiment.require.php'),
+  'Footer'                   => array('File' , 'Code/footer.php'),
+  'Header'                   => array('File' , 'Code/Header.php'),
+  'Icon'                     => array('File' , 'Code/icon.png'),
+  'Initiate Collector'       => array('File' , 'Code/initiateCollector.php'),
+  'Trial Validator Require'  => array('File' , 'Code/trialValidator.require.php'),
+  'No JS'                    => array('File' , 'Code/nojs.php'),
+  'Set Password'             => array('File' , 'Code/setPassword.php'),
+  'Shuffle Functions'        => array('File' , 'Code/shuffleFunctions.php'),
+  'Custom Functions'         => array('File' , 'Code/customFunctions.php'),
+
+  'CSS Dir'                  => array('Dir'  , 'Code/css/'),
+    'Global CSS'             => array('File' , 'Code/css/global.css'),
+    'Jquery UI CSS'          => array('File' , 'Code/css/jquery-ui.min.css'),
+    'Jquery UI Custom CSS'   => array('File' , 'Code/css/jquery-ui-1.10.4.custom.min.css'),
+    'No JS Warning'          => array('File' , 'Code/css/nojswarning.png'),
+    'Tools CSS'              => array('File' , 'Code/css/tools.css'),
+    'Tutorial CSS'           => array('File' , 'Code/css/tutorial.css'),
+    'Fonts'                  => array('Dir'  , 'Code/css/fonts/'),
+    'CSS Images'             => array('Dir'  , 'Code/css/images/'),
+
+  'Classes'                  => array('Dir'  , 'Code/classes/'),
+    'Conditions Class'       => array('File' , 'Code/classes/ConditionController.php'),
+    'Control Files Class'    => array('File' , 'Code/classes/ControlFile.php'),
+    'Debug Class'            => array('File' , 'Code/classes/DebugController.php'),
+    'Errors Class'           => array('File' , 'Code/classes/ErrorController.php'),
+    'Pathfinder'             => array('File' , 'Code/classes/Pathfinder.php'),
+    'Procedure Class'        => array('File' , 'Code/classes/Procedure.php'),
+    'Return Class'           => array('File' , 'Code/classes/ReturnVisitController.php'),
+    'SideData Class'         => array('File' , 'Code/classes/SideData.php'),
+    'Status Class'           => array('File' , 'Code/classes/StatusController.php'),
+    'Stimuli Class'          => array('File' , 'Code/classes/Stimuli.php'),
+    'system map'             => array('File' , 'Code/classes/systemMap.php'),
+    'Trial Validator Class'  => array('File' , 'Code/classes/systemMap.php'),
+    'Users Class'            => array('File' , 'Code/classes/User.php'),
+
+  'JS Dir'                   => array('Dir'  , 'Code/javascript/'),
+    'Collector JS'           => array('File' , 'Code/javascript/Collector.js'),
+    'Jquery'                 => array('File' , 'Code/javascript/jquery.js'),
+    'Jquery UI Custom'       => array('File' , 'Code/javascript/jquery-ui-1.10.4.custom.min.js'),
+    'Jquery UI'              => array('File' , 'Code/javascript/jquery-1.12.4.min.js'),
+    'Login JS'               => array('File' , 'Code/javascript/loggingIn.js'),
+    'Sha256 JS'              => array('File' , 'Code/javascript/sha256.js'),
+
+'Data'                       => array('Dir'  , 'Data/'),
+  'Current Data Dir'         => array('Dir'  , 'Data/[Current Experiment]-Data[Data Sub Dir]/'),
+    'Instructions Data'      => array('File' , 'Data/[Current Experiment]-Data[Data Sub Dir]/InstructionsData.csv'),
+    'Status Begin Data'      => array('ioCSV', 'Data/[Current Experiment]-Data[Data Sub Dir]/Status_Begin.csv'),
+    'Status End Data'        => array('ioCSV', 'Data/[Current Experiment]-Data[Data Sub Dir]/Status_End.csv'),
+    'SideData Data'          => array('ioCSV', 'Data/[Current Experiment]-Data[Data Sub Dir]/SideData.csv'),
+    'Counter Dir'            => array('Dir'  , 'Data/[Current Experiment]-Data[Data Sub Dir]/Counter/'),
+      'Counter'              => array('File' , 'Data/[Current Experiment]-Data[Data Sub Dir]/Counter/Counter.csv'),
+    'Session Storage Dir'    => array('Dir'  , 'Data/[Current Experiment]-Data[Data Sub Dir]/session/'),
+      'Session Storage'      => array('---'  , 'Data/[Current Experiment]-Data[Data Sub Dir]/session/[Username].txt'),
+    'Output Dir'             => array('Dir'  , 'Data/[Current Experiment]-Data[Data Sub Dir]/Output/'),
+      'Experiment Output'    => array('ioCSV', 'Data/[Current Experiment]-Data[Data Sub Dir]/Output/[Output]'),
+        'Trial Validations Dir'     => array('kVal', 'Data/[Current Experiment]-Data[Data Sub Dir]/validations/'),
+        'Trial Validation Scan Time'=> array('File', 'Data/[Current Experiment]-Data[Data Sub Dir]/validations/'),
+
+'Admin'                      => array('Dir' , 'Admin/'),
+  'Admin Index'              => array('File', 'Admin/index.php'),
+  'Tools'                    => array('Dir' , 'Admin/Tools/'),
+
+'Trial Types'                => array('Dir' , 'TrialTypes/'),
+  'Trial Type Dir'           => array('Dir' , 'TrialTypes/[var]/'),
+    'Trial Display'          => array('File', 'TrialTypes/[var]/display.php'),
+    'Trial Helper'           => array('File', 'TrialTypes/[var]/helper.php'),
+    'Trial Scoring'          => array('File', 'TrialTypes/[var]/scoring.php'),
+    'Trial Script'           => array('File', 'TrialTypes/[var]/script.js'),
+    'Trial Style'            => array('File', 'TrialTypes/[var]/style.css'),
+    'Trial Validator'        => array('File', 'TrialTypes/[var]/validator.php'),
+  'default helper'           => array('File', 'TrialTypes/defaultTrialHelper.php'),
+  'default scoring'          => array('File', 'TrialTypes/defaultTrialScoring.php'),
 );
