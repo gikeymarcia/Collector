e68dabf3df55da76c6f2518fdf7f11109bb78520
gikeymarcia <gikeymarcia@gmail.com>
2016-09-14 16:32:24 -0700

    turning the following trials into templates:
    WordPair,
    WordPairDelayed,
    Cue
    CuedRecall
    fontsize,
    FreeRecall,
    Instruct,
    Passage,

--- TrialTypes/FreeRecall/display.php
+++
@@ -1,8 +0,0 @@
-<div>
-  <div class="shim"><?= $_EXPT->get('text') ?></div>
-  <textarea class="fr-textarea collectorInput" name="Response" wrap="physical" value=""></textarea>
-</div>
-
-<div class="textright">
-  <button class="collectorButton collectorAdvance" id="FormSubmitButton">Submit</button>
-</div>
