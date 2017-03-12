e77d0058ed85035887f180fe2af0d7897af8203b
anthonyhaffey <anthony.haffey@gmail.com>
2017-03-12 19:41:58 +0000

    Admin-> Sim-> GUI: All elements except for questionnaire ones (checkbox and Radio) can be created with GUI.

--- Admin/Tools/Simulator/GUI/GUINewElements.js
+++ Admin/Tools/Simulator/GUI/GUINewElements.js
@@ -5,12 +5,65 @@ new_element_template = {
       return new_text_element;
     }
   },
-  
-  /*
-  image: {
     
+  Image: {
+    create_element:function(elementNumber,this_location){
+      
+      //use generic image like e-prime does          
+      
+      var new_img_element = "<img id='element"+elementNumber+"' src='https://dl.dropbox.com/s/m5g0e6zfk1kiqwb/Picture.png?dl=0' style='"+this_location+"width:100px;height:100px;'>";
+      return new_img_element;
+    }
   },
-  */
+  Audio: {
+    create_element:function(elementNumber,this_location){
+      
+      //use generic image like e-prime does          
+      
+      var new_img_element = "<img id='element"+elementNumber+"' src='https://dl.dropbox.com/s/1ayf8oyr31qbn5q/Audio.png?dl=0' style='"+this_location+"width:100px;height:100px;'>";
+      return new_img_element;
+    }
+  },
+  Video: {
+    create_element:function(elementNumber,this_location){
+      
+      //use generic image like e-prime does          
+      
+      var new_img_element = "<img id='element"+elementNumber+"' src='https://dl.dropbox.com/s/ilssbb4a5ikmn6i/Movie.png?dl=0' style='"+this_location+"width:100px;height:100px;'>";
+      return new_img_element;
+    }
+  },
+  Button: {    
+    create_element:function(elementNumber,this_location){
+      var new_text_element = "<input type='button' id='element"+elementNumber+"' style='"+this_location+"' value='Button'>";
+      return new_text_element;
+    }
+  },
+  String: {
+    create_element:function(elementNumber,this_location){
+      var new_text_element = "<input type='string' id='element"+elementNumber+"' style='"+this_location+"' placeholder='Text/String'>";
+      return new_text_element;
+    }
+  },
+  Number: {
+    create_element:function(elementNumber,this_location){
+      var new_text_element = "<input type='string' id='element"+elementNumber+"' style='"+this_location+"'>";
+      return new_text_element;
+    }
+  },
+  Date: {
+    create_element:function(elementNumber,this_location){
+      var new_text_element = "<input type='Date' id='element"+elementNumber+"' style='"+this_location+"'>";
+      return new_text_element;
+    }
+  },
+  Checkbox:{
+    placeholder:0
+  },
+  Radio:{
+    placeholder:0
+  }
+  
   
   
 }
