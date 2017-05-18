83495982c7626963a22b342f29c5312e257bfe2f
anthonyhaffey <anthony.haffey@gmail.com>
2017-05-18 19:04:39 +0100

    Admin:DownloadFiles -> Basic tool to allow downloading of individual files

---
+++ Admin/Tools/DownloadFiles/download.php
@@ -0,0 +1,23 @@
+<?php
+
+    
+    $file_url = $_POST['fileurl'];
+    echo $file_url;
+    $file_split = explode('/',$file_url);
+    echo"<br>";
+    $pp = $file_split[count($file_split)-2];
+    $experiment = $file_split[count($file_split)-3];
+    
+    //
+    
+    $this_basename =  basename($file_url);
+    
+    
+    header('Content-Type: application/octet-stream');
+    header("Content-Transfer-Encoding: Binary"); 
+    
+    header("Content-disposition: attachment; filename='".$experiment."_".$pp."_".$this_basename."'"); 
+    readfile($file_url); // do the double-download-dance (dirty but worky)
+    
+
+?>
---
+++ Admin/Tools/DownloadFiles/index.php
@@ -0,0 +1,26 @@
+<form method='post' action='download.php'>                
+<?php
+    require '../../initiateTool.php';
+    
+    $data_path = $FILE_SYS->get_path("Data");
+    
+    echo $data_path;
+    // list directories in pathinfo
+    $studies_array =array_diff(scandir($data_path),array('..', '.','index.php','sess'));
+    
+    foreach($studies_array as $study){
+        echo "<h2>$study</h2>";
+        echo "<h3>participants</h3>";
+        $participants = array_diff(scandir("$data_path/$study"),array('..', '.','RandomAssignments.txt'));
+        print_r($participants);
+        foreach($participants as $participant){
+            echo "<h4>$participant</h4>";
+            $these_files = array_diff(scandir("$data_path/$study/$participant"),array('..', '.','RandomAssignments.txt'));
+            foreach($these_files as $this_file){
+                chmod("$data_path/$study/$participant/$this_file",0444);
+                echo "<button name='fileurl' value='$data_path/$study/$participant/$this_file'>$this_file</button><br>";
+            }
+        }
+    }
+?>
+</form>
