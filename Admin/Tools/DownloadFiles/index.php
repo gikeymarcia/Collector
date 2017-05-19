40d3c412f22e51fdc210b00efa577328fb0c5e53
AnthonyHaffey <anthony.haffey@gmail.com>
2017-05-19 11:08:23 +0100

    Admin->Tools->Tidy up DownloadFiles index.php

--- Admin/Tools/DownloadFiles/index.php
+++ Admin/Tools/DownloadFiles/index.php
@@ -4,7 +4,7 @@
     
     $data_path = $FILE_SYS->get_path("Data");
     
-    echo $data_path;
+    //echo $data_path;
     // list directories in pathinfo
     $studies_array =array_diff(scandir($data_path),array('..', '.','index.php','sess'));
     
@@ -12,13 +12,13 @@
         echo "<h2>$study</h2>";
         echo "<h3>participants</h3>";
         $participants = array_diff(scandir("$data_path/$study"),array('..', '.','RandomAssignments.txt'));
-        print_r($participants);
+        //print_r($participants);
         foreach($participants as $participant){
             echo "<h4>$participant</h4>";
             $these_files = array_diff(scandir("$data_path/$study/$participant"),array('..', '.','RandomAssignments.txt'));
             foreach($these_files as $this_file){
                 chmod("$data_path/$study/$participant/$this_file",0444);
-                echo "<button name='fileurl' value='$data_path/$study/$participant/$this_file'>$this_file</button><br>";
+                echo "<button class='collectorButton' name='fileurl' value='$data_path/$study/$participant/$this_file'>$this_file</button><br>";
             }
         }
     }
