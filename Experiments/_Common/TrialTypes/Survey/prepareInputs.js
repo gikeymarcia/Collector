f673aa7d25237166c438e4b33992d5c6345a1506
anthonyhaffey <anthony.haffey@gmail.com>
2017-06-20 19:30:37 +0100

    Surveys within Collector can be accessed by typing in their filenames in "Text" column

--- Experiments/_Common/TrialTypes/Survey/prepareInputs.js
+++ Experiments/_Common/TrialTypes/Survey/prepareInputs.js
@@ -1,1 +1,8 @@
-survey_address = Trial.get_input('Text');<!-- Warn if try to edit with GUI !-->
+survey_address = Trial.get_input('Text');
+
+alert(survey_address.indexOf("https"));
+
+if(survey_address.indexOf("https") == -1){
+    survey_address = "../../../Experiments/_Common/Surveys/"+survey_address;
+} 
+<!-- Warn if try to edit with GUI !-->
