cc4d1734d87392f7577759969ab1ac03bb09157a
anthonyhaffey <anthony.haffey@gmail.com>
2017-06-28 17:45:37 +0100

    errors in previous survey commit
    tidying up radio input for surveys

--- Code/classes/AjaxSurvey.php
+++ Code/classes/AjaxSurvey.php
@@ -1,33 +1,28 @@
 <?php
 
-//require '../../Admin/initiateTool.php';
-//ob_end_clean(); // no need to transmit useless data
-//ini_set('html_errors', false);
+require '../../Code/initiateCollector.php';
+ob_end_clean(); // no need to transmit useless data
+ini_set('html_errors', false);
 
 $file             = $_POST['file'];
 
+
 //check the filename is a csv
-/*
-if ($file === '' OR preg_match('/[^a-zA-Z0-9._ -]/', $file) !== 0) {
-    exit('error: invalid json name: "' . $file . '"');
+
+if ($file === '' OR preg_match('/[^a-zA-Z0-9._ -?]/', $file) !== 0) {
+    exit('error: invalid file name: "' . $file . '"');
 } else {
-    */
-if (strtolower(substr($file, -4)) === '.csv') {
-    // that is good
-  } else {
-    $file_exploded=explode(".",$file);
-    if(count($file_exploded)>1){
-      exit("error: invalid file type submitted"); // might be a bit harsh, maybe automatically rename
+    if (strtolower(substr($file, -4)) === '.csv') {
+        // that is fine
     } else {
-      $file = "$file.csv";
+        $file = "$file.csv";        
     }
-  }
-//}
+}
 
-//$survey_dir = $FILE_SYS-> get_path("Surveys");
+$survey_dir = $FILE_SYS-> get_path("Surveys");
 
-if(strpos($file,"http") == 0){
-    $file_location = $file;    
+if(strpos($file,"http") !== false){
+    $file_location = "$file";    
 } else {
     $file_location =  "$survey_dir/$file";    
 }
@@ -35,9 +30,5 @@ if(strpos($file,"http") == 0){
 $file_contents = file_get_contents ($file_location);
 
 echo $file_contents;
-
-//echo ("$file,$data,$experiment_name,$read_write");
-
-
 
 ?>
