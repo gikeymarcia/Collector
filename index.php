810728a44a360e4ec9b6d6756a27d521b3026be1
anthonyhaffey <anthony.haffey@gmail.com>
2017-07-12 19:15:23 +0100

    starting to write code for adding sql databases

--- index.php
+++ index.php
@@ -18,6 +18,56 @@
  */
 require 'Code/initiateCollector.php';
 
+
+
+    $servername     = "localhost";
+    $username       = "anthony";
+    $password       = "HVIg1Xg6XChmYb33";
+    $database_name  = "Collector_Users";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password,$database_name);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    
+    
+    
+    /*
+    
+    // sql to create table
+    $sql = "CREATE TABLE Users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    email VARCHAR(50),    
+    reg_date TIMESTAMP
+    )";
+
+    
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Table  created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+    $conn->close();
+    */
+    
+    
+    /*
+    // Create database
+    $sql = "CREATE DATABASE $database_name";
+    if ($conn->query($sql) === TRUE) {
+        echo "Database created successfully";
+    } else {
+        echo "Error creating database: " . $conn->error;
+    }
+ 
+    $conn->close();
+   */
+
+
 if(isset($_SESSION['username'])){    
     print_r($_SESSION['username']);
     $login_style = "display:none";
