5f6e1839c4f7b061fc08ba16979e91fe9564042a
anthonyhaffey <anthony.haffey@gmail.com>
2017-07-20 16:42:54 +0100

    First implementation of "skins", that Collector has three interfaces:
    1) Collector - online experiments
    2) CoLecture - Teaching
    3) Apps - Other  (Which may or may not use the typical data structure of Collector)

--- index.php
+++ index.php
@@ -75,6 +75,15 @@ if(isset($_SESSION['username'])){
     $login_style = "";
 }
 
+if(isset($_POST['skin'])){
+    $_SESSION['skin'] = $_POST['skin']; 
+}
+
+
+if(!isset($_SESSION['skin'])){
+    $_SESSION['skin'] = "Collector";
+}
+
 // get possible experiments to choose from
 $experiments = array();
 $exp_folder  = $FILE_SYS->get_path('Experiments');
@@ -84,32 +93,51 @@ foreach (get_Collector_experiments($FILE_SYS) as $exp_name) {
 
 output_page_header($FILE_SYS, 'Collector Homepage');
 
+$apps = array();
+
 
 ?>
 
 <style>
-  .inlineUL { display: inline-block; margin: auto; text-align: left; }
-  #header_bar{
-    position:fixed;
-    right:0px;
-    z-index:5;
-    width:100%;
-    background-color:white;
-    top:0px;
-    left:0px;
-    padding: 10px;
-    border-bottom-style: solid;
-    border-bottom-width: 2px;
-    border-bottom-color: blue;
-    box-shadow: -1px 2px 5px grey;  
-  }
-  #username_input{
-      width:400px;
-  }
+    .inlineUL { display: inline-block; margin: auto; text-align: left; }
+  
+    .interface_div {
+        display:none;
+    }
+  
+    #header_bar{
+        position:fixed;
+        right:0px;
+        z-index:5;
+        width:100%;
+        background-color:white;
+        top:0px;
+        left:0px;
+        padding: 10px;
+        box-shadow: -1px 2px 5px grey;  
+    }
+    #username_input{
+        width:400px;
+    }
+    #skin_span{
+        position:absolute;
+        left:10px;
+    }
 
 </style>
 <form action="index.php" method="post">
     <div id="header_bar" align="right">
+        <span id='skin_span'>
+            <span id="Collector_skin"   class="skin_button" value="Collector"></span>
+            <span id="CoLecture_skin"   class="skin_button" value="CoLecture"></span>
+            <span id="Apps_skin"        class="skin_button" value="Apps"></span>    
+        </span>
+        
+        <span>
+            <span> Batteries of tasks ... or way to sub group tasks </span>
+            <span> other tools for allowing users to select courses etc. </span>
+        </span>
+        
         <span id="username"></span>
         <input id="username_input" type="email" placeholder="e-mail as username (may not be necessary)">
         <input type="button" id="register_button" value="register" style="<?= $login_style ?>">
@@ -118,25 +146,63 @@ output_page_header($FILE_SYS, 'Collector Homepage');
     </div>
 </form>
 
-
 <div class="collectorRoot">
-    <h1>Collector</h1>
-    <h2>A program for running experiments on the web</h2>
 
-    <p>Welcome to the Collector. If you would like to begin an experiment,
-       click on one of the links below.
-    </p>
+    <div id="Collector_div" class="interface_div">
 
-    <ul class="inlineUL">
-      <?php foreach ($experiments as $name => $path): ?>
-      <li><a href='<?= $path ?>'><?= $name ?></a></li>
-      <?php endforeach; ?>
-    </ul>
+        <h1>Collector</h1>
+        <h2>A program for running experiments on the web</h2>
 
+        <p>Welcome to the Collector. If you would like to begin an experiment,
+           click on one of the links below.
+        </p>
+    
+    
+        <ul class="inlineUL">
+          <?php foreach ($experiments as $name => $path): ?>
+          <li><a href='<?= $path ?>'><?= $name ?></a></li>
+          <?php endforeach; ?>
+        </ul>
+        
+    </div>
+
+    <div id="CoLecture_div" class="interface_div">
+    
+        here's where the CoLectures will be
+        
+    </div>
+    
+    <div id="Apps_div" class="interface_div">
+    
+        here's where the apps will be
+        
+    </div>
+        
     <p>Otherwise, you can access one of the other tools
        <a href="<?= $FILE_SYS->get_path('Admin') ?>">here</a>.
     </p>
 </div>
+
+
+<script>
+
+var current_skin = "<?= $_SESSION['skin'] ?>";
+
+$(".skin_button").each(function(i,obj){
+    console.dir(i);
+    console.dir(obj.id);
+    var this_value = obj.id.replace("_skin","");
+    if(obj.id == current_skin+"_skin"){
+        $("#"+obj.id).html("<em><b>"+this_value+"</b></em>"); 
+        $("#"+this_value+"_div").show();
+    } else {
+        $("#"+obj.id).html("<input type='submit' name='skin' class='collectorButton' value='"+this_value+"'>");
+    }
+});
+
+</script>
+
+
 
 <?php
 output_page_footer($FILE_SYS);
