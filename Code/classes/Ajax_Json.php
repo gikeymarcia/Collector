22a8d02ee792f6233e1a059e1b848ac1a20e4d81
anthonyhaffey <anthony.haffey@gmail.com>
2017-07-26 11:55:33 +0100

    update of Ajax_Json to distinguish between app and json updates

--- Code/classes/Ajax_Json.php
+++ Code/classes/Ajax_Json.php
@@ -7,49 +7,59 @@ ini_set('html_errors', false);
 
 $file             = $_POST['file'];
 $data             = $_POST['data'];
-
-$read_write_list       = $_POST['read_write_list'];
+$read_write_list  = strtolower($_POST['read_write_list']);
+$json_app         = strtolower($_POST['json_app']);
 
 //check the filename is a json
-if ($file === '' OR preg_match('/[^a-zA-Z0-9._ -]/', $file) !== 0) {
-    exit('error: invalid json name: "' . $file . '"');
+
+if($json_app == "app"){
+    // do checks here
 } else {
-  if (strtolower(substr($file, -5)) === '.json') {
-    // that is good
-  } else {
-    $file_exploded=explode(".",$file);
-    if(count($file_exploded)>1){
-      exit("error: invalid file type submitted"); // might be a bit harsh, maybe automatically rename
+    
+    if ($file === '' OR preg_match('/[^a-zA-Z0-9._ -]/', $file) !== 0) {
+        exit('error: invalid file name: "' . $file . '"');
     } else {
-      $file = "$file.json";
+      if (strtolower(substr($file, -5)) === '.json') {
+        // that is good
+      } else {
+        $file_exploded=explode(".",$file);
+        if(count($file_exploded)>1){
+          exit("error: invalid file type submitted"); // might be a bit harsh, maybe automatically rename
+        } else {
+          $file = "$file.json";
+        }
+      }
     }
-  }
+
 }
 
-$file_directory = $FILE_SYS-> get_path("Jsons");
-$file_location  = "$file_directory/$file";
-if($read_write_list == "Read"){
+
+if($json_app == "json"){
+    $file_directory = $FILE_SYS-> get_path("Jsons");
+    $file_location  = "$file_directory/$file";    
+}
+
+if($json_app == "app"){
+    $file_directory = $FILE_SYS-> get_path("Apps");
+    $file_location  = "$file_directory/$file";    
+} 
+
+if($read_write_list == "read"){
     echo file_get_contents($file_location);
 } 
-if($read_write_list == "Write"){
+if($read_write_list == "write"){
     file_put_contents($file_location,$data);
     echo $file_location;
     echo $data;
 }
-if($read_write_list == "List"){
-    $json_path = $FILE_SYS-> get_path("Jsons");
-    $file = str_replace(".json","",$file);
-    //echo "$json_path/$file"."*.json";
-    //$files = glob("$json_path/$file"."*.json");
-    $files = glob("$json_path/Mousetracker*");
+if($read_write_list == "list"){
+    $files = glob("$file_directory/*");
     foreach($files as &$file){
         $filename = explode("/",$file);
         $file = $filename[count($filename) -1];        
     }
     echo json_encode($files);
-
 }
-
 
 
 //echo ("$file,$data,$experiment_name,$read_write");
